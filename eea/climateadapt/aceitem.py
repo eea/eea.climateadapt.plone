7bc2fe0787a10762fe6a944b95ca82c4e64f5564
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-07-16 15:15:10 +0300

    More fields in the AceItem

--- eea/climateadapt/aceitem.py
+++ eea/climateadapt/aceitem.py
@@ -2,7 +2,8 @@ from eea.climateadapt import MessageFactory as _
 from plone.directives import dexterity, form
 from plone.namedfile.interfaces import IImageScaleTraversable
 from zope.interface import implements
-from zope.schema import Choice
+from zope.schema import Choice, TextLine, List, Bool, Int, Text
+
 
 #from five import grok
 # from zope import schema
@@ -20,14 +21,11 @@ from zope.schema import Choice
 #
 # from z3c.relationfield.schema import RelationList, RelationChoice
 # from plone.formwidget.contenttree import ObjPathSourceBinder
-#
-#
 
-# Interface class; used to define content-type schema.
 
 class IAceItem(form.Schema, IImageScaleTraversable):
     """
-    Generic Ace Item
+    Defines content-type schema for Ace Item
     """
 
     # form.model("models/aceitem.xml")
@@ -39,6 +37,65 @@ class IAceItem(form.Schema, IImageScaleTraversable):
     storage_type = Choice(title=_(u"Storage Type"),
                        required=True,
                        vocabulary="eea.climateadapt.aceitems_storagetypes")
+
+    spatial_layer = TextLine(title=_(u"Spatial Layer"),
+                             required=False,
+                             default=u""
+                             )
+
+    sectors = List(title=_(u"Sectors"),
+                   description=_(u"TODO: Sectors description here"),
+                   required=False,
+                   value_type=Choice(
+                       vocabulary="eea.climateadapt.aceitems_sectors",
+                   ),
+                   )
+
+    elements = List(title=_(u"Elements"),
+                    description=_(u"TODO: Elements description here"),
+                    required=False,
+                    value_type=Choice(
+                        vocabulary="eea.climateadapt.aceitems_elements",
+                    ),
+                    )
+
+    climate_impacts = List(title=_(u"Climate impacts"),
+                    description=_(u"TODO: Climate impacts description here"),
+                    required=False,
+                    value_type=Choice(
+                        vocabulary="eea.climateadapt.aceitems_climateimpacts",
+                    ),
+                    )
+
+    important = Bool(title=_(u"Important"), required=False, default=False)
+    source = TextLine(title=_(u"Source"), required=True,)
+    comments = TextLine(title=_(u"Comments"), required=False, default=u"")
+    year = Int(title=_(u"Year"), required=True)
+    geochars = Text(title=_(u"Geochars"), required=False, default=u"")
+
+    # TODO: see if possible to use eea.promotions for this
+    featured = List(title=_(u"Featured in location"),
+                   description=_(u"TODO: Featured description here"),
+                   required=False,
+                   value_type=Choice(
+                       vocabulary="eea.climateadapt.aceitems_featured",
+                   ),
+                   )
+
+    # TODO: rating??? seems to be manually assigned, not computed
+
+    # TODO: storedat: can contain a related measure or project, or a URL
+    # if contains inner contents, starts with ace_project_id=<id>
+    # or ace_measure_id=<id>
+
+    #supdocs - this is a related field. It seems to point to dlfileentry
+
+    # replacesid - tot un related??
+
+    # scenario: only 3 items have a value: "SCENES SUE", "SCENES ECF", "IPCCS",
+    # IPCCSRES A1B
+    # the options are stored in a AceItemScenario constant in Java code
+
 
 class AceItem(dexterity.Item):
     implements(IAceItem)
--- eea/climateadapt/vocabulary.py
+++ eea/climateadapt/vocabulary.py
@@ -17,6 +17,25 @@ def generic_vocabulary(_terms):
     return factory
 
 
+def vocab_from_labels(text):
+    def factory(context):
+        """ AceItem data types """
+
+        terms = []
+        for line in filter(None, text.split('\n')):
+            first, label = line.split('=')
+            name = first.split('-lbl-')[1]
+            terms.append((name, label))
+
+        terms.sort(cmp=lambda x,y: cmp(x[1], y[1]))
+
+        return SimpleVocabulary([
+            SimpleTerm(n, n, l) for n, l in terms
+        ])
+
+    return factory
+
+
 # changes title and buttons (what to add) in view for AceItem
 # extracted from JAVA code:
 _datatypes = """
@@ -31,23 +50,9 @@ acesearch-datainfotype-lbl-ORGANISATION=Organisations
 acesearch-datainfotype-lbl-MEASURE=Adaptation options
 acesearch-datainfotype-lbl-ACTION=Case studies
 """
-
-def aceitem_datatypes_vocabulary(context):
-    """ AceItem data types """
-
-    terms = []
-    for line in filter(None, _datatypes.split('\n')):
-        first, label = line.split('=')
-        name = first.split('acesearch-datainfotype-lbl-')[1]
-        terms.append((name, label))
-
-    terms.sort(cmp=lambda x,y: cmp(x[1], y[1]))
-
-    return SimpleVocabulary([
-        SimpleTerm(n, n, l) for n, l in terms
-    ])
-
+aceitem_datatypes_vocabulary = vocab_from_labels(_datatypes)
 alsoProvides(aceitem_datatypes_vocabulary, IVocabularyFactory)
+
 
 # changes how the view for AceItem behaves (what it shows)
 _storagetypes = [
@@ -58,7 +63,52 @@ _storagetypes = [
     "SETOFMAPS",
     "MEASURE",
 ]
-
 aceitem_storagetypes_vocabulary = generic_vocabulary(_storagetypes)
 alsoProvides(aceitem_storagetypes_vocabulary, IVocabularyFactory)
 
+
+_sectors = """
+acesearch-sectors-lbl-AGRICULTURE=Agriculture and Forest
+acesearch-sectors-lbl-BIODIVERSITY=Biodiversity
+acesearch-sectors-lbl-COASTAL=Coastal areas
+acesearch-sectors-lbl-DISASTERRISKREDUCTION=Disaster Risk Reduction
+acesearch-sectors-lbl-FINANCIAL=Financial
+acesearch-sectors-lbl-HEALTH=Health
+acesearch-sectors-lbl-INFRASTRUCTURE=Infrastructure
+acesearch-sectors-lbl-URBAN=Urban
+acesearch-sectors-lbl-MARINE=Marine and Fisheries
+acesearch-sectors-lbl-WATERMANAGEMENT=Water management
+"""
+aceitem_sectors_vocabulary = vocab_from_labels(_sectors)
+alsoProvides(aceitem_sectors_vocabulary, IVocabularyFactory)
+
+
+_elements = """
+acesearch-elements-lbl-OBSERVATIONS=Observations and Scenarios
+acesearch-elements-lbl-VULNERABILITY=Vulnerability Assessment
+acesearch-elements-lbl-ACTION=Adaptation Actions
+acesearch-elements-lbl-PLANSTRATEGY=Adaptation Plans and Strategies
+acesearch-elements-lbl-EU_POLICY=Sector Policies
+acesearch-elements-lbl-MEASUREACTION=Adaptation Measures and Actions
+"""
+aceitem_elements_vocabulary = vocab_from_labels(_elements)
+alsoProvides(aceitem_elements_vocabulary, IVocabularyFactory)
+
+
+_climateimpacts = """
+aceitem-climateimpacts-lbl-EXTREMETEMP=Temperatures
+aceitem-climateimpacts-lbl-WATERSCARCE=Water Scarcity
+aceitem-climateimpacts-lbl-FLOODING=Flooding
+aceitem-climateimpacts-lbl-SEALEVELRISE=Sea Level Rise
+aceitem-climateimpacts-lbl-DROUGHT=Droughts
+aceitem-climateimpacts-lbl-STORM=Storms
+aceitem-climateimpacts-lbl-ICEANDSNOW=Ice and Snow
+"""
+aceitem_climateimpacts_vocabulary = vocab_from_labels(_climateimpacts)
+alsoProvides(aceitem_climateimpacts_vocabulary, IVocabularyFactory)
+
+
+_featured = [('CASEHOME', 'Feature this on the homepage'),
+             ('CASESEARCH', 'Feature this on study search results page')]
+aceitem_featured_vocabulary = generic_vocabulary(_featured)
+alsoProvides(aceitem_featured_vocabulary, IVocabularyFactory)
