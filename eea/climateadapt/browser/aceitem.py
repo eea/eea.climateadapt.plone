ae165b5b70d547ee907e0b36266578ded9cd126c
Taygun <taygun.agiali@eaudeweb.ro>
2015-08-03 10:40:50 +0200

    Refs #27720 - Created default view for content types derived from AceItem

--- eea/climateadapt/browser/aceitem.py
+++ eea/climateadapt/browser/aceitem.py
@@ -1,6 +1,37 @@
-#from Products.Five import BrowserView
+# from Products.Five import BrowserView
 from plone.dexterity.browser.view import DefaultView
+
 
 class AceItemView(DefaultView):
     def __call__(self):
         return super(AceItemView, self).__call__()
+
+
+class PublicationReportView(DefaultView):
+
+    def __call__(self):
+        return super(PublicationReportView).__call__()
+
+
+class InformationPortalView(DefaultView):
+
+    def __call__(self):
+        return super(InformationPortalView).__call__()
+
+
+class GuidanceDocumentView(DefaultView):
+
+    def __call__(self):
+        return super(GuidanceDocumentView).__call__()
+
+
+class ToolView(DefaultView):
+
+    def __call__(self):
+        return super(ToolView).__call__()
+
+
+class OrganizationView(DefaultView):
+
+    def __call__(self):
+        return super(OrganizationView).__call__()
