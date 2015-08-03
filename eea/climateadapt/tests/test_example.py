56b7899e22f6f5077ffd4cfc1720c3fac7811165
Taygun <taygun.agiali@eaudeweb.ro>
2015-08-03 16:57:21 +0200

    Refs #27720 - Created schema tests for content types derived from AceItem

--- eea/climateadapt/tests/test_example.py
+++ eea/climateadapt/tests/test_example.py
@@ -1,12 +1,17 @@
 import unittest2 as unittest
 
 from Products.CMFCore.utils import getToolByName
+from plone.dexterity.interfaces import IDexterityFTI
+from zope.component import queryUtility
 
 from eea.climateadapt.testing import \
     EEA_CLIMATEADAPT_INTEGRATION_TESTING
+from eea.climateadapt.aceitem import \
+    IPublicationReport, IInformationPortal, IGuidanceDocument, \
+    ITool, IOrganization
 
 
-class TestExample(unittest.TestCase):
+class IntegrationTest(unittest.TestCase):
 
     layer = EEA_CLIMATEADAPT_INTEGRATION_TESTING
 
@@ -23,3 +28,28 @@ class TestExample(unittest.TestCase):
         installed = [p['id'] for p in self.qi_tool.listInstalledProducts()]
         self.assertTrue(pid in installed,
                         'package appears not to have been installed')
+
+    def test_schema_publication_report(self):
+        fti = queryUtility(IDexterityFTI, name='PublicationReport')
+        schema = fti.lookupSchema()
+        self.assertEqual(IPublicationReport, schema)
+
+    def test_schema_information_potal(self):
+        fti = queryUtility(IDexterityFTI, name='IInformationPortal')
+        schema = fti.lookupSchema()
+        self.assertEqual(IInformationPortal, schema)
+
+    def test_schema_guidance_document(self):
+        fti = queryUtility(IDexterityFTI, name='GuidanceDocument')
+        schema = fti.lookupSchema()
+        self.assertEqual(IGuidanceDocument, schema)
+
+    def test_schema_tool(self):
+        fti = queryUtility(IDexterityFTI, name='Tool')
+        schema = fti.lookupSchema()
+        self.assertEqual(ITool, schema)
+
+    def test_schema_organization(self):
+        fti = queryUtility(IDexterityFTI, name='Organization')
+        schema = fti.lookupSchema()
+        self.assertEqual(IOrganization, schema)
