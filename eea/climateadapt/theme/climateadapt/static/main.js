fc7552bbde2abdb363325d39cca0f6f5c7594350
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-12-15 17:18:40 +0200

    Refs #31422 CCA; implemented navigation menu

---
+++ eea/climateadapt/theme/climateadapt/static/main.js
@@ -0,0 +1,11 @@
+(function($){
+	$(document).ready(function(){
+        console.log("asta este");
+		$('ul.dropdown-menu [data-toggle=dropdown]').on('click', function(event) {
+			event.preventDefault(); 
+			event.stopPropagation(); 
+			$(this).parent().siblings().removeClass('open');
+			$(this).parent().toggleClass('open');
+		});
+	});
+})(jQuery);
