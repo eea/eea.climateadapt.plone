a759591b3525ed272486013a8f4467b4e5663f93
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-12-23 17:59:46 +0200

    Work on faceted navigation page

--- eea/climateadapt/facetedsearch.py
+++ eea/climateadapt/facetedsearch.py
@@ -1,6 +1,8 @@
 """ Utilities for faceted search
 """
 
+from Products.Five.browser import BrowserView
+from collections import defaultdict
 from zope.interface import alsoProvides
 from zope.schema.interfaces import IVocabularyFactory
 from zope.schema.vocabulary import SimpleTerm
@@ -32,3 +34,42 @@ def search_types_vocabulary(context):
     ])
 
 alsoProvides(search_types_vocabulary, IVocabularyFactory)
+
+
+FACETED_SEARCH_TYPES = [
+    ("CITYPROFILE", "Mayors Adapt city profiles"),
+    ("COVER", "Content in Climate-ADAPT"),
+    ("DOCUMENT", "Publication & Report"),
+    ("INFORMATIONSOURCE", "Information Portal"),
+    ("GUIDANCE","Guidance"),
+    ("TOOL", "Tools"),
+    ("MAPGRAPHDATASET", "Maps, graphs and datasets"),
+    ("INDICATOR", "Indicators"),
+    ("RESEARCHPROJECT","Research and knowledge Projects"),
+    #("MEASURE","Adaptation Option"),
+    ("ACTION", "Case Studies"),
+    ("ORGANISATION", "Organisations"),
+]
+
+
+class ListingView(BrowserView):
+    """ Faceted listing view for ClimateAdapt
+    """
+
+    @property
+    def sections(self):
+        return [x[0] for x in FACETED_SEARCH_TYPES]
+
+    @property
+    def labels(self):
+        return dict(FACETED_SEARCH_TYPES)
+
+    def results(self, batch):
+        results = defaultdict(lambda:[])
+        for brain in batch:
+            if brain.search_type:
+                if brain.search_type in self.labels:
+                    results[brain.search_type].append(brain)
+
+        print results
+        return results
