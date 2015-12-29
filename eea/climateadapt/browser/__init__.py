cec5fd10774c2ff90744fe935407244eef2c1ea3
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2015-12-29 16:24:22 +0200

    Use code to generate the navbar menu

--- eea/climateadapt/browser/__init__.py
+++ eea/climateadapt/browser/__init__.py
@@ -1,3 +1,4 @@
+from Products.Five.browser import BrowserView
 import json
 
 #
@@ -95,3 +96,88 @@ class AceViewApi(object):
             return (
                 "http://adapt-test.eea.europa.eu/viewmeasure?ace_measure_id=%s"
                 % self.context._acemeasure_id)
+
+
+class Navbar(BrowserView):
+    """ The global site navbar
+    """
+
+    _menu = """
+        Home        /
+
+        Adaptation information              /adaptation-information/general
+            General                         /adaptation-information/general
+            Observations and scenarios      /observations-and-scenarios
+            Vulnerabilities and risks       /vulnerabilities-and-risks
+            Adaptation options              /adaptation-measures
+            Adaptation strategies           /adaptation-strategies
+            Research projects               /research-projects
+            Uncertainty guidance            /uncertainty-guidance-ai
+
+        EU Adaptation policy            /eu-adaptation-policy/landing
+            EU adaptation policy and funding   /eu-adaptation-policy/landing
+            EU Adaptation Strategy      /eu-adaptation-policy/strategy
+            Mayors Adapt                /mayors-adapt
+            EU sector policies          /eu-adaptation-policy/mainstreaming
+            EU funding of adaptation    /eu-adaptation-policy/funding
+
+        Countries, regions, cities      /countries/general
+            General                     /countries/general
+            Countries                   /countries
+            Transnational regions       /transnational-regions
+            Cities and towns            /cities
+
+        Tools                                   /tools/general
+            General                             /tools/general
+            Adaptation support tool             /adaptation-support-tool
+            Case study search tool              /sat
+            Map viewer                          /tools/map-viewer
+            Uncertainty guidance                /uncertainty-guidance
+            Guidelines for project managers     /guidelines-for-project-managers
+            Urban vulnerability mapbook         /tools/urban-adaptation
+            Urban adaptation support tool       /tools/urban-ast
+            Time series tool                    /tools/time-series-tool
+
+        Links       /organisations
+            Organisations       /organisations
+            Global platforms    /international
+
+        Search the database    /data-and-downloads
+
+        Newsletter      /newsletter
+    """
+
+    def menu(self):
+        lines = self._menu.split('\n')
+        sections = []
+        this_section = None
+        for line in lines:
+            line = line.strip()
+
+            # empty lines at beginning of docstring
+            if not line and this_section is None:
+                continue
+
+            # new section
+            if not line and this_section:
+                sections.append(this_section)
+                this_section = None
+                continue
+
+            # new section
+            if line and this_section is None:
+                lpos = line.find('/')
+                label, link = line[:lpos].strip(), line[lpos:].strip()
+                this_section = [label, link, []]
+                continue
+
+            # link inside section
+            if line and this_section:
+                lpos = line.find('/')
+                label, link = line[:lpos].strip(), line[lpos:].strip()
+                this_section[2].append((label, link))
+
+        sections.append(this_section)
+
+        return sections
+
