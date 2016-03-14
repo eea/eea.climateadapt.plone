4cc1a0f4aab6748dbc0b673f527c1d1a8b0c7626
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-03-14 13:09:14 +0200

    Fix link handling in aceprojects

--- eea/climateadapt/browser/aceproject.py
+++ eea/climateadapt/browser/aceproject.py
@@ -5,3 +5,7 @@ from eea.climateadapt.browser import AceViewApi
 class AceProjectView(DefaultView, AceViewApi):
     def __call__(self):
         return super(AceProjectView, self).__call__()
+
+    def linkify(self, text):
+        if not text.startswith('http'):
+            return "http://" + text
