dab78bbb47f35d6b84aa7979ab28e2812858bfa0
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-04-26 19:10:57 +0300

    31236 CCA; improved the editing interface of aceitems

--- eea/climateadapt/aceitem.py
+++ eea/climateadapt/aceitem.py
@@ -30,10 +30,96 @@ class IAceItem(form.Schema, IImageScaleTraversable):
     Defines content-type schema for Ace Item
     """
 
-    title = TextLine(title=_(u"Title"), required=True)
-
     dexteritytextindexer.searchable('long_description')
-    long_description = RichText(title=(u"description"), required=True)
+
+    form.fieldset('default',
+        label=u'Item Description',
+        fields=['title', 'long_description', 'keywords', 'sectors',
+                'climate_impacts', 'elements', 'year']
+    )
+
+    form.fieldset('reference_information',
+        label=u'Reference information',
+        fields=['websites', 'source']
+    )
+
+    # TODO:
+    # form.fieldset('reference_information',
+    #     label=u'Documents',
+    #     fields=[]
+    # )
+
+    form.fieldset('geographic_information',
+        label=u'Geographic Information',
+        fields=['geochars', 'comments']
+    )
+
+    form.fieldset('backend',
+        label=u'Backend fields',
+        fields=[]
+    )
+
+    # -----------[ "default" fields ]------------------
+
+    title = TextLine(title=_(u"Title"),
+                     description=u"Item Name (250 character limit)",
+                     required=True)
+
+    long_description = RichText(title=(u"description"),
+                                description=u"Provide a description of the item. (5,000 character limit)",
+                                required=True)
+
+    # TODO: "keyword" from SQL is Subject
+    keywords = RichText(title=_(u"Keywords"),
+                        description=_(u"Describe and tag this item with relevant keywords. Separate each keyword with a comma. For example, example keyword 1, example keyword 2 (1,000 character limit)"),
+                        required=False)
+
+    sectors = List(title=_(u"Sectors"),
+                   description=_(u"Select one or more relevant sector policies that this item relates to."),
+                   required=False,
+                   value_type=Choice(
+                       vocabulary="eea.climateadapt.aceitems_sectors",),
+                   )
+
+    climate_impacts = List(
+        title=_(u"Climate impacts"),
+        description=_(u"Select one or more climate change impact topics that this item relates to."),
+        required=False,
+        value_type=Choice(
+            vocabulary="eea.climateadapt.aceitems_climateimpacts",),
+    )
+
+    elements = List(title=_(u"Elements"),
+                    description=_(u"Select one or more elements."),
+                    required=False,
+                    value_type=Choice(
+                        vocabulary="eea.climateadapt.aceitems_elements",),
+                    )
+
+    year = Int(title=_(u"Year"), description=u"Date of publication/release/update of the item", required=True)
+
+    # -----------[ "reference_information" fields ]------------------
+
+    websites = RichText(title=_(u"Websites"),
+                        description=u'List the Name and Website where the item can be found or is described. (500 character limit). Please separate each website with semicolon.',
+                        required=True,)
+
+    source = RichText(title=_(u"Source"),
+                      required=True,
+                      description=u"Describe the original source of the item description (250 character limit)")
+
+    # -----------[ "geographic_information" fields ]------------------
+
+    geochars = Text(title=_(u"Geographic characterisation"),
+                    required=False,
+                    default=u"",
+                    description=u"Select the characterisation for this item",
+                    )
+
+    comments = TextLine(title=_(u"Comments"), required=False, default=u"",
+                        description=u"Comments about this database item [information entered below will not be displayed on the public pages of climate-adapt]")
+
+    # -----------[ "backend" fields ]------------------
 
     # fix???
     data_type = Choice(title=_(u"Data Type"),
@@ -44,11 +130,6 @@ class IAceItem(form.Schema, IImageScaleTraversable):
     storage_type = Choice(title=_(u"Storage Type"),
                           required=True,
                           vocabulary="eea.climateadapt.aceitems_storagetypes")
-
-    # TODO: "keyword" from SQL is Subject
-    keywords = RichText(title=_(u"Keywords"),
-                        description=_(u"Keywords related to the project"),
-                        required=False)
 
     spatial_layer = TextLine(title=_(u"Spatial Layer"),
                              required=False,
@@ -61,54 +142,22 @@ class IAceItem(form.Schema, IImageScaleTraversable):
                           value_type=Choice(
                               vocabulary="eea.climateadapt.ace_countries"))
 
-    sectors = List(title=_(u"Sectors"),
-                   description=_(u"TODO: Sectors description here"),
-                   required=False,
-                   value_type=Choice(
-                       vocabulary="eea.climateadapt.aceitems_sectors",),
-                   )
-
-    elements = List(title=_(u"Elements"),
-                    description=_(u"TODO: Elements description here"),
-                    required=False,
-                    value_type=Choice(
-                        vocabulary="eea.climateadapt.aceitems_elements",),
-                    )
-
-    climate_impacts = List(
-        title=_(u"Climate impacts"),
-        description=_(u"TODO: Climate impacts description here"),
-        required=False,
-        value_type=Choice(
-            vocabulary="eea.climateadapt.aceitems_climateimpacts",),
-    )
-
     important = Bool(title=_(u"High importance"), required=False,
                      default=False)
 
     # websites = List(title=_(u"Websites"),
     #                 required=True,
     #                 value_type=TextLine(title=_(u"Link"), ))
-    websites = RichText(title=_(u"Websites"), required=True,)
-
-    source = RichText(title=_(u"Source"), required=True,)
-
     metadata = RichText(title=_(u"Metadata"), required=False,)
 
-    comments = TextLine(title=_(u"Comments"), required=False, default=u"")
 
-    year = Int(title=_(u"Year"), required=True)
-
-    geochars = Text(title=_(u"Geographic characterisation"),
-                    required=False, default=u"")
-
-    # TODO: see if possible to use eea.promotions for this
-    featured = List(title=_(u"Featured in location"),
-                    description=_(u"TODO: Featured description here"),
-                    required=False,
-                    value_type=Choice(
-                        vocabulary="eea.climateadapt.aceitems_featured",),
-                    )
+    # # TODO: see if possible to use eea.promotions for this
+    # featured = List(title=_(u"Featured in location"),
+    #                 description=_(u"TODO: Featured description here"),
+    #                 required=False,
+    #                 value_type=Choice(
+    #                     vocabulary="eea.climateadapt.aceitems_featured",),
+    #                 )
 
     special_tags = List(title=_(u"Special tagging"),
                         required=False,
