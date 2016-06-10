70b6dc7c7a7fdb03b0baee10f7cc893e8c170adb
Petchesi-Iulian <iamnotacat12@gmail.com>
2016-06-10 14:59:28 +0300

    Refs #73286 CCA: Moved the category field to adaptations options only.

--- eea/climateadapt/browser/adaptationoption.py
+++ eea/climateadapt/browser/adaptationoption.py
@@ -1,8 +1,14 @@
 from plone.dexterity.browser.view import DefaultView
 from eea.climateadapt.browser import AceViewApi
+from plone.z3cform.fieldsets.extensible import FormExtender
 
 
 class AdaptationOptionView(DefaultView, AceViewApi):
     """ """
 
     type_label = u"Adaptation option"
+
+
+class AdaptationOptionEditFormExtender(FormExtender):
+    def update(self):
+        self.move('category', before='stakeholder_participation')
