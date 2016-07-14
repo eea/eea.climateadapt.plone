25e16443fc925cafc53edb1b335979e0dd1ad166
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-07-14 18:26:50 +0300

    Refs #73320 CCA; better force unlock of objects

--- eea/climateadapt/browser/admin.py
+++ eea/climateadapt/browser/admin.py
@@ -81,20 +81,22 @@ class ForceUnlock(BrowserView):
 
     def __call__(self):
         annot = getattr(self.context, '__annotations__', {})
+
+        if hasattr(self.context, '_dav_writelocks'):
+            del self.context._dav_writelocks
+            self.context._p_changed = True
         if 'plone.locking' in annot:
             del annot['plone.locking']
 
             self.context._p_changed = True
             annot._p_changed = True
 
-            url = self.context.absolute_url()
-            props_tool = getToolByName(self.context, 'portal_properties')
-            if props_tool:
-                types_use_view = \
-                    props_tool.site_properties.typesUseViewActionInListings
-                if self.context.portal_type in types_use_view:
-                    url += '/view'
+        url = self.context.absolute_url()
+        props_tool = getToolByName(self.context, 'portal_properties')
+        if props_tool:
+            types_use_view = \
+                props_tool.site_properties.typesUseViewActionInListings
+            if self.context.portal_type in types_use_view:
+                url += '/view'
 
-            return self.request.RESPONSE.redirect(url)
-        else:
-            return "done"
+        return self.request.RESPONSE.redirect(url)
