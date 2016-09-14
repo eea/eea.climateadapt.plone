fdd5a20710c16844d265bc45c6e4b26a4dda8f14
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-09-14 14:11:41 +0300

    Refs #74389 CCA; manually trigger IWorkingCopyDeletedEvent to avoid dangling locked objects

---
+++ eea/climateadapt/events.py
@@ -0,0 +1,19 @@
+from plone.app.contentrules.handlers import execute_rules
+from plone.app.iterate.dexterity.utils import get_baseline
+from plone.app.iterate.event import WorkingCopyDeletedEvent
+from zope.event import notify
+
+
+def trigger_contentrules(event):
+    execute_rules(event)
+
+
+def handle_iterate_wc_deletion(object, event):
+    """ When a WorkingCopy is deleted, the problem was that the locking was not
+    removed. We're manually triggering the IWorkingCopyDeletedEvent because
+    the plone.app.iterate handler is registered for IWorkingCopyRelation, a
+    derivate of Archetype's relations, which is not used in the dexterity
+    implementation.
+    """
+    baseline = get_baseline(object)
+    notify(WorkingCopyDeletedEvent(object, baseline, relation=None) )
--- eea/climateadapt/utils.py
+++ eea/climateadapt/utils.py
@@ -2,12 +2,6 @@
 """
 
 import time
-from plone.app.contentrules.handlers import execute_rules
-
-
-def trigger_contentrules(event):
-    execute_rules(event)
-
 
 def _unixtime(d):
     """ Converts a datetime to unixtime
