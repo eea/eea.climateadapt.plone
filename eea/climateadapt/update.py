b40c97c4b31cc7e963be0e777af38aa44506dd82
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-09-20 16:41:29 +0300

    Refs #75968 CCA; cleanup in update code

--- eea/climateadapt/update.py
+++ eea/climateadapt/update.py
@@ -16,79 +16,23 @@ def update_to_8(context):
 
     site = context.getSite()
 
-    fix_covers(site)
-    fix_content(site)
+    _fix_covers(site)
+    _fix_content(site)
 
 
 def update_to_9(context):
     if context.readDataFile('eea.climateadapt.update_9.txt') is None:
         return
 
-    fix_content(context.getSite())
+    _fix_content(context.getSite())
 
 
-def fix_content(site):
+def _fix_content(site):
     """ Fix the tags in all objects in the site
     """
+    # TODO: rename this function, needs better name
 
-    for brain in search_catalog(site):
-        obj = aq_self(brain.getObject())
-
-        if not (hasattr(obj, 'special_tags') or hasattr(obj, 'specialtagging')):
-            continue
-
-        tags = []
-        for attr in ['special_tags', 'specialtagging']:
-            st = getattr(obj, attr, []) or []
-            if isinstance(st, basestring):
-                tags.append(st)
-            else:
-                tags.extend(st)
-
-        tags = fix_tags(tags)
-
-        if tags:
-            logger.info("Fixing tags on %s", brain.getURL())
-            obj.special_tags = tags
-            obj.reindexObject()
-
-
-
-def fix_covers(self):
-    """ Fix tags in all cover tiles
-    """
-
-    covers = self.portal_catalog.searchResults(
-                          portal_type='collective.cover.content')
-
-    for cover in covers:
-        cover = cover.getObject()
-        if hasattr(cover, '__annotations__'):
-            for tile_id in list(cover.__annotations__.keys()):
-                tile_id = tile_id.encode()
-
-                if 'plone.tiles.data' in tile_id:
-                    tile = cover.__annotations__[tile_id]
-                    if 'special_tags' and 'search_text' in tile.keys():
-                        tile['special_tags'] = fix_tags(tile['special_tags'])
-                        tile['search_text'] = fix_tags(tile['search_text'])
-                        tile._p_changed = True
-                        cover.reindexObject()
-
-
-def fix_tags(tags):
-    if isinstance(tags, (list, tuple)):
-        tags = [i.replace('-', '_') for i in tags]
-    elif tags:
-        tags = tags.replace('-', '_')
-
-    return list(set(filter(None, tags)))
-
-
-def search_catalog(self):
-    """  Perform a catalog search
-    """
-    catalog = self.portal_catalog
+    catalog = site.portal_catalog
 
     searchTypes = [
         'eea.climateadapt.aceproject',
@@ -105,4 +49,56 @@ def search_catalog(self):
     ]
     results = catalog.searchResults({'portal_type': searchTypes})
 
-    return results
+    for brain in results:
+        obj = aq_self(brain.getObject())
+
+        if not (hasattr(obj, 'special_tags') or hasattr(obj, 'specialtagging')):
+            continue
+
+        tags = []
+        for attr in ['special_tags', 'specialtagging']:
+            st = getattr(obj, attr, []) or []
+            if isinstance(st, basestring):
+                tags.append(st)
+            else:
+                tags.extend(st)
+
+        tags = _fix_tags(tags)
+
+        if tags:
+            logger.info("Fixing tags on %s", brain.getURL())
+            obj.special_tags = tags
+            obj.reindexObject()
+
+
+def _fix_covers(self):
+    """ Fix tags in all cover tiles
+    """
+    # TODO: rename this function, needs better name
+
+    covers = self.portal_catalog.searchResults(
+                          portal_type='collective.cover.content')
+
+    for cover in covers:
+        cover = cover.getObject()
+        if hasattr(cover, '__annotations__'):
+            for tile_id in list(cover.__annotations__.keys()):
+                tile_id = tile_id.encode()
+
+                if 'plone.tiles.data' in tile_id:
+                    tile = cover.__annotations__[tile_id]
+                    if 'special_tags' and 'search_text' in tile.keys():
+                        tile['special_tags'] = _fix_tags(tile['special_tags'])
+                        tile['search_text'] = _fix_tags(tile['search_text'])
+                        tile._p_changed = True
+                        cover.reindexObject()
+
+
+def _fix_tags(tags):
+    # TODO: rename this function, needs better name
+    if isinstance(tags, (list, tuple)):
+        tags = [i.replace('-', '_') for i in tags]
+    elif tags:
+        tags = tags.replace('-', '_')
+
+    return list(set(filter(None, tags)))
