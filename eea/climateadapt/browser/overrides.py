2962a5cd9c59054fffe30d01221e0c187b81b95e
Petchesi-Iulian <iamnotacat12@gmail.com>
2016-11-01 10:53:21 +0200

    Refs #77133 CCA: Tinymce tabs override.

--- eea/climateadapt/browser/overrides.py
+++ eea/climateadapt/browser/overrides.py
@@ -6,6 +6,17 @@ from plone.app.content.browser.interfaces import IContentsPage
 from plone.memoize.instance import memoize
 from Products.CMFPlone import utils
 
+from plone.app.widgets.dx import RichTextWidget
+from eea.climateadapt.interfaces import IEEAClimateAdaptInstalled
+from z3c.form.interfaces import IFieldWidget
+from z3c.form.widget import FieldWidget
+from zope.component import adapter
+from zope.interface import implementer
+from z3c.form.util import getSpecification
+from plone.app.contenttypes.behaviors.richtext import IRichText  # noqa
+from z3c.form.interfaces import IFormLayer
+from plone.app.widgets.interfaces import IWidgetsLayer
+
 
 class DisplaySubMenuItem(DSMI):
     """ Override because we have covers with id 'index_html' and we want to
@@ -27,3 +38,44 @@ class DisplaySubMenuItem(DSMI):
             return False
         else:
             return False
+
+
+class OverrideRichText (RichTextWidget):
+    """ Richtext field override for tinymce tabs plugin """
+
+    def _base_args(self):
+        # Get options
+        args = super(OverrideRichText, self)._base_args()
+
+        # Get tinymce options
+        tinyoptions = args['pattern_options']['tiny']
+        buttons = 'tabs tabsDelete tabsItemDelete tabsItemInsertAfter tabsItemInsertBefore  '
+        toolbar = tinyoptions['toolbar']
+        plugins = tinyoptions['plugins']
+
+        # Modify toolbar
+        toolbar = toolbar.split('|')
+        toolbar[5] = toolbar[5] + buttons
+        toolbar = '|'.join(toolbar)
+
+        # Override
+        args['pattern_options']['tiny']['theme_advanced_buttons3'] = buttons
+        args['pattern_options']['tiny']['toolbar'] = toolbar
+        args['pattern_options']['tiny']['plugins'].append('tabs')
+
+        return args
+
+    def render(self):
+        return super(OverrideRichText, self).render()
+
+
+@adapter(getSpecification(IRichText['text']), IWidgetsLayer)
+@implementer(IFieldWidget)
+def RichTextFieldWidget(field, request):
+    return FieldWidget(field, OverrideRichText(request))
+
+
+@adapter(getSpecification(IRichText['text']), IFormLayer)
+@implementer(IFieldWidget)
+def RichTextFieldWidgett(field, request):
+    return FieldWidget(field, OverrideRichText(request))
