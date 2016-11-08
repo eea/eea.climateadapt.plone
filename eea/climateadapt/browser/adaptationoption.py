353f5d521a7c2279d6b8b3e6f7fea00622a0e8e0
Petchesi-Iulian <iamnotacat12@gmail.com>
2016-11-08 11:52:43 +0200

    Refs #77574 CCA: Added related case studies to adaptation option view.

--- eea/climateadapt/browser/adaptationoption.py
+++ eea/climateadapt/browser/adaptationoption.py
@@ -1,17 +1,40 @@
+from Acquisition import aq_inner
 from eea.climateadapt.browser import AceViewApi
 from plone.dexterity.browser.add import DefaultAddForm
 from plone.dexterity.browser.edit import DefaultEditForm
 from plone.dexterity.browser.view import DefaultView
 from plone.dexterity.interfaces import IDexterityEditForm
-from plone.z3cform.fieldsets.extensible import FormExtender
 from plone.z3cform import layout
+from plone.z3cform.fieldsets.extensible import FormExtender
+from zc.relation.interfaces import ICatalog
+from zope.component import getUtility
 from zope.interface import classImplements
+from zope.intid.interfaces import IIntIds
+from zope.security import checkPermission
 
 
 class AdaptationOptionView(DefaultView, AceViewApi):
     """ """
 
     type_label = u"Adaptation option"
+
+    def get_related_casestudies(self):
+        titles = []
+        urls = []
+
+        catalog = getUtility(ICatalog)
+        intids = getUtility(IIntIds)
+        result = []
+        for rel in catalog.findRelations(
+                    dict(to_id=intids.getId(aq_inner(self.context)),
+                         from_attribute='adaptationoptions')
+                ):
+            obj = intids.queryObject(rel.from_id)
+            if obj is not None and checkPermission('zope2.View', obj):
+                titles.append(obj.title)
+                urls.append(obj.absolute_url())
+
+        return {'url': urls, 'title': titles}
 
 
 class AdaptationOptionFormExtender(FormExtender):
