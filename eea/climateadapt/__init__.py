97cf18b65b276fb5258a0677e20d4793b0ffb26f
Tiberiu Ichim <tiberiu@pixelblaster.ro>
2016-11-10 16:14:59 +0200

    Refs #75995 CCA; fix problem with editors pasting content in folder where they're not allowed to change folder

--- eea/climateadapt/__init__.py
+++ eea/climateadapt/__init__.py
@@ -13,6 +13,35 @@ collective.cover.config.DEFAULT_GRID_SYSTEM = 'bootstrap3'
 from plone.i18n import normalizer
 normalizer.MAX_URL_LENGTH = 100
 
+# monkey patch plone.dexterity.content.Container
+from plone.dexterity.content import Container
+from AccessControl import ClassSecurityInfo
+from AccessControl.class_init import InitializeClass
+import Products.CMFCore.permissions
+# Change permission for manage_pasteObjects to "Add portal content"
+# See https://dev.plone.org/ticket/9177
+
+# TODO: Find a way to do this without patching __ac_permissions__ directly
+
+def drop_protected_attr_from_ac_permissions(attribute, classobj):
+    new_mappings = []
+    for mapping in Container.__ac_permissions__:
+        perm, attrs = mapping
+        if not attribute in attrs:
+            new_mappings.append(mapping)
+        else:
+            modified_attrs = tuple([a for a in attrs if not a == attribute])
+            modified_mapping = (perm, modified_attrs)
+            new_mappings.append(modified_mapping)
+    classobj.__ac_permissions__ = tuple(new_mappings)
+
+drop_protected_attr_from_ac_permissions('manage_pasteObjects', Container)
+sec = ClassSecurityInfo()
+sec.declareProtected(Products.CMFCore.permissions.AddPortalContent,
+                    'manage_pasteObjects')
+sec.apply(Container)
+InitializeClass(Container)
+
 
 def initialize(context):
 
@@ -23,3 +52,4 @@ def initialize(context):
                               roleplugin.manage_addCityMayorUserFactory),
                           visibility=None
                           )
+
