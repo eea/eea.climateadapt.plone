deff5553ccd6440988e4a0fa630262df04eafd4a
Petchesi-Iulian <iamnotacat12@gmail.com>
2017-05-18 11:16:03 +0300

    Refs #85220 CCA: Enable new slide for banner.

--- eea/climateadapt/browser/tilehelpers.py
+++ eea/climateadapt/browser/tilehelpers.py
@@ -42,33 +42,33 @@ class ICarousel(IPersistentCoverTile):
     """ Frontpage carousel tile schema """
 
     # Slide 1 fields
-    # s1_title = schema.Text(title=u"First slide Title", required=True)
-    # s1_description = RichText(title=u"First slide description", required=False)
+    s1_title = schema.Text(title=u"First slide Title", required=True)
+    s1_description = RichText(title=u"First slide description", required=False)
 
-    # s1_primary_photo = NamedBlobImage(
-    #     title=(u"First Slide Photo"),
-    #     required=False,
-    # )
+    s1_primary_photo = NamedBlobImage(
+        title=(u"First Slide Photo"),
+        required=False,
+    )
 
-    # s1_photo_copyright = schema.Text(title=u"Photo Copyright", required=True)
+    s1_photo_copyright = schema.Text(title=u"Photo Copyright", required=True)
 
-    # s1_read_more_text = schema.Text(title=u"First slide read more text",
-    #                                 required=False)
-    # s1_read_more_link = schema.Text(title=u"First slide read more link",
-    #                                 required=False)
+    s1_read_more_text = schema.Text(title=u"First slide read more text",
+                                    required=False)
+    s1_read_more_link = schema.Text(title=u"First slide read more link",
+                                    required=False)
 
     # Slide 2 fields
-    s2_title = schema.Text(title=u"First slide Title", required=True)
-    s2_description = RichText(title=u"First slide description", required=False)
+    s2_title = schema.Text(title=u"Second slide Title", required=True)
+    s2_description = RichText(title=u"Second slide description", required=False)
 
     s2_primary_photo = NamedBlobImage(
-        title=(u"First Slide Photo"),
+        title=(u"Second Slide Photo"),
         required=True,
     )
 
-    s2_read_more_text = schema.Text(title=u"First slide read more text",
+    s2_read_more_text = schema.Text(title=u"Second slide read more text",
                                     required=False)
-    s2_read_more_link = schema.Text(title=u"First slide read more link",
+    s2_read_more_link = schema.Text(title=u"Second slide read more link",
                                     required=False)
 
     # Slide 5 fields
