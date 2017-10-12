18358bfdef5a85a8e3518a9fde24843b64826bc3
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2017-10-12 12:08:35 +0300

    Refs #83426 CCA; sync workflows from production

---
+++ eea/climateadapt/profiles/default/workflows/newsevents_workflow/scripts/unexpire.py
@@ -0,0 +1,10 @@
+## Script (Python) "unexpire"
+##bind container=container
+##bind context=context
+##bind namespace=
+##bind script=script
+##bind subpath=traverse_subpath
+##parameters=change
+##title=
+##
+change.object.setExpirationDate(None)
