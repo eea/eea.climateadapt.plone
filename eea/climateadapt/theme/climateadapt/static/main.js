f683aae60c0b799b7f2ca59d59d6ab92bcb8747d
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2017-10-23 12:19:46 +0300

    Refs #89126; Fix AST links

--- eea/climateadapt/theme/climateadapt/static/main.js
+++ eea/climateadapt/theme/climateadapt/static/main.js
@@ -269,15 +269,11 @@ function fix_map(){
 
     // Open all links out of the tool in a new window
     if ($('.subsection-tools-urban-ast').length) {
-      $.each($('#content-core').find('a'), function (index, value){
-        $(value).attr('target', '_blank');
-      })
+      $('.cover-richtext-tile ul li a').attr('target', '_blank');
     }
 
     if ($('.subsection-tools-adaptation-support-tool').length) {
-      $.each($('#content-core').find('a'), function (index, value){
-        $(value).attr('target', '_blank');
-      })
+      $('.cover-richtext-tile ul li a').attr('target', '_blank');
     }
 
     // Urban ast export section pdf button
