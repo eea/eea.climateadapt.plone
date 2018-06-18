10bac23b8def0694acf307d8bd52d864a448f455
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-06-18 12:47:47 +0300

    Refs #96217 Add new slide in the frontpage carousel.

--- eea/climateadapt/browser/tilehelpers.py
+++ eea/climateadapt/browser/tilehelpers.py
@@ -68,11 +68,11 @@ class ICarousel(IPersistentCoverTile):
                           's5_read_more_text', 's5_read_more_link']
                   )
 
-    # form.fieldset('slide7',
-    #               label=u'Slide 7',
-    #               fields=['s7_title', 's7_description', 's7_primary_photo',
-    #                       's7_read_more_text', 's7_read_more_link']
-    #               )
+    form.fieldset('slide7',
+                  label=u'Slide 7',
+                  fields=['s7_title', 's7_description', 's7_primary_photo',
+                          's7_read_more_text', 's7_read_more_link']
+                  )
 
     # Slide 1 fields
     # s1_title = schema.Text(title=u"First slide Title", required=True)
@@ -121,18 +121,18 @@ class ICarousel(IPersistentCoverTile):
                                     required=False)
 
     # Slide 7 fields
-    # s7_title = schema.Text(title=u"Seventh slide title", required=True)
-    # s7_description = RichText(title=u"Seventh slide text", required=False)
+    s7_title = schema.Text(title=u"Seventh slide title", required=True)
+    s7_description = RichText(title=u"Seventh slide text", required=False)
 
-    # s7_primary_photo = NamedBlobImage(
-    #     title=(u"Seventh slide photo"),
-    #     required=True,
-    # )
+    s7_primary_photo = NamedBlobImage(
+        title=(u"Seventh slide photo"),
+        required=True,
+    )
 
-    # s7_read_more_text = schema.Text(title=u"Seventh slide read more text",
-    #                                 required=False)
-    # s7_read_more_link = schema.Text(title=u"Seventh slide read more link",
-    #                                 required=False)
+    s7_read_more_text = schema.Text(title=u"Seventh slide read more text",
+                                    required=False)
+    s7_read_more_link = schema.Text(title=u"Seventh slide read more link",
+                                    required=False)
 
     # Slide 8 fields
     s8_title = schema.Text(title=u"Slide title", required=True)
