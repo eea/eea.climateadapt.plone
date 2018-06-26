50eb193040e5e4fc7c17bdb0cbbcce1a514bc951
Petchesi-Iulian <iamnotacat12@gmail.com>
2018-06-26 14:32:19 +0300

    Refs #93475 Modified the broken links script so that it gets more results.

--- eea/climateadapt/browser/misc.py
+++ eea/climateadapt/browser/misc.py
@@ -6,6 +6,7 @@ import transaction
 
 from Acquisition import aq_inner
 from BeautifulSoup import BeautifulSoup
+from eea.climateadapt import MessageFactory as _
 from eea.climateadapt.config import CONTACT_MAIL_LIST
 from eea.climateadapt.schema import Email
 from email.MIMEText import MIMEText
@@ -430,6 +431,47 @@ def _archive_news(site):
             transaction.commit()
 
 
+def convert_to_string(item):
+    """ Convert to string other types
+    """
+    if not item:
+        return ''
+    if not isinstance(item, basestring):
+        new_item = ""
+        try:
+            iterator = iter(item)
+        except TypeError, err:
+            value = getattr(item, 'raw', None)
+            if value:
+                return value
+            logger.error(err)
+            return ''
+        else:
+            for i in iterator:
+                new_item += i
+        return new_item
+    return item
+
+
+def discover_links(string_to_search):
+    """ Use regular expressions to get all urls in string
+    """
+    # REGEX = re.compile(ur'(?i)\b((?:https?://|www\d{0,3}[.]|[a-z0-9.\-]+[.]
+    # [a-z]{2,4}/)(?:[^\s()<>]|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>
+    # ]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:\'\".,<>?\xab\xbb\u201c\u201d\u2018
+    # \u2019]))')
+    REGEX = re.compile('http[s]?://(?:[a-zA-Z]|[0-9]|[$-_@.&+]|[!*\(\),]|(?:%[0-9a-fA-F][0-9a-fA-F]))+')
+
+    try:
+        result = re.findall(REGEX, string_to_search) or []
+        if isinstance(result, basestring):
+            result = [result]
+    except Exception, err:
+        logger.error(err)
+        result = []
+    return result
+
+
 def compute_broken_links(site):
     """ Script that will get called by cron once per day
     """
@@ -478,7 +520,8 @@ def get_links(site):
         'link': link,
         'object_url': path
     })
-
+    count = 0
+    logger.info('Got %s objects' % len(brains))
     for b in brains:
         obj = b.getObject()
         path = obj.getPhysicalPath()
@@ -491,22 +534,43 @@ def get_links(site):
         else:
             if obj.portal_type == 'eea.climateadapt.city_profile':
                 append_urls(obj.website_of_the_local_authority, path)
+        attrs = ['long_description', 'description', 'source', 'comments']
+        for attr in attrs:
+            string_to_search = convert_to_string(getattr(obj, attr, ''))
 
-            elif obj.portal_type == 'collective.cover.content':
-                for tile in obj.list_tiles():
-                    if 'richtext' in obj.get_tile_type(tile):
-                        richtext = obj.get_tile(tile).getText()
-                        bs = BeautifulSoup(richtext)
-                        links = bs.findAll(
-                            'a', attrs={'href': re.compile("^https?://")}
-                        )
-                        for link in links:
-                            append_urls(link.get('href'), path)
-            else:
-                logger.info("Portal type: %s" % obj.portal_type)
+            if len(string_to_search) > 0:
+                if attr == 'long_description':
+                    bs = BeautifulSoup(string_to_search)
+                    links = bs.findAll(
+                        'a', attrs={'href': re.compile("^https?://")}
+                    )
+
+                    for link in links:
+                        append_urls(link.get('href'), path)
+                else:
+                    links = discover_links(string_to_search)
+
+                    # get rid of duplicates
+                    links = list(set(links))
+                    for link in links:
+                        append_urls(link, path)
+
+        if obj.portal_type == 'collective.cover.content':
+            for tile in obj.list_tiles():
+                if 'richtext' in obj.get_tile_type(tile):
+                    richtext = obj.get_tile(tile).getText()
+                    bs = BeautifulSoup(richtext)
+                    links = bs.findAll(
+                        'a', attrs={'href': re.compile("^https?://")}
+                    )
+                    for link in links:
+                        append_urls(link.get('href'), path)
+
+        count += 1
+        if count % 100 == 0:
+            logger.info('Finished going through %s objects' % count)
 
     logger.info("Finished getting links.")
-
     return urls
 
 
@@ -519,8 +583,11 @@ def check_link(link):
         if isinstance(link, unicode):
             link = link.encode()
 
-        if link[0:7].find('http') == -1:
-            link = 'http://' + link
+        try:
+            if link[0:7].find('http') == -1:
+                link = 'http://' + link
+        except Exception, err:
+            logger.error(err)
 
         logger.info("LINK: %s", link)
         try:
