0e491c4596046fa1a5227cb829fec08f06455097
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2018-07-18 17:13:03 +0300

    Refs #97142; fix navigation toggle on mobile

--- eea/climateadapt/theme/climateadaptv2/src/js/script.js
+++ eea/climateadapt/theme/climateadaptv2/src/js/script.js
@@ -53,16 +53,15 @@ $(document).ready(function() {
     });
   }
 
-
   // HOMEPAGE: Tabs functionality
-  $("ul.nav-tabs a").click(function(e) {
-    e.preventDefault();
+  $('ul.nav-tabs a').click(function(e) {
     $(this).tab('show');
+    e.preventDefault();
   });
 
   // HOMEPAGE: Dynamic area:
   // on click center tab items on small screen sizes
-  $(".main-nav-tabs li a").click(function() {
+  $('.main-nav-tabs li a').click(function() {
     var $parent = $(this).parent();
     centerTabItem($parent, '.main-tab-heading .main-nav-tabs');
   });
@@ -85,7 +84,6 @@ $(document).ready(function() {
     }, 500);
   }
 
-
   // HOMEPAGE: Dynamic area - Getting started:
   // On button hover toggle class on the bubble section
   $('.action-btn').each(function() {
@@ -101,7 +99,6 @@ $(document).ready(function() {
       }
     })
   })
-
 
   // HOMEPAGE: Dynamic area - Adaptation support tool:
   // Highlight steps on hover
@@ -140,7 +137,7 @@ $(document).ready(function() {
   });
 
   // mobile menu button on click event
-  $('.mobile-menu i').on('click', function() {
+  $('.mobile-menu i').click(function() {
     $('body').toggleClass('no-ovf');
     $(this).toggleClass('fa-bars fa-times');
     $('.header').toggleClass('mobile-header');
@@ -150,10 +147,8 @@ $(document).ready(function() {
   });
 
   // mobile - show submenus on click
-  $('.angle-down-icon').on('click', function() {
+  $('.angle-down-icon').click(function() {
     $(this).parent().siblings('.sub-menu-wrapper').toggle();
-    $(this).parent().siblings('li').find('.sub-menu-wrapper').hide();
-    return false;
   });
 
   $('.personal-menu-action a').click(function() {
@@ -161,26 +156,30 @@ $(document).ready(function() {
     $('.login-container ').slideToggle();
   });
 
+  // Navigation menu: align sub-menu to the right
+  // if overflows the main navigation menu
   var mainMenuWidth = $('.main-nav').width();
 
   $('.main-nav li').mouseenter(function() {
-    var subMenuWidth = $(this).children(".sub-menu-wrapper").width();
-    if ($(this).find('div.sub-menu-wrapper').length > 0) {
-      var subMenuLeft = $(this).children(".sub-menu-wrapper").offset().left;
+    var subMenuWidth = $(this).children('.sub-menu-wrapper').width();
+    if ($(this).find('.sub-menu-wrapper').length > 0) {
+      var subMenuLeft = $(this).children('.sub-menu-wrapper').offset().left;
     }
 
     if (mainMenuWidth - (subMenuWidth + subMenuLeft) < 0) {
-      $(this).children(".sub-menu-wrapper").css({
+      $(this).children('.sub-menu-wrapper').css({
         'right': 0,
         'left': 'auto',
       });
     }
   });
 
+  // Navigation menu: divide the content
+  // in sub-menu in 2 columns if 'sub-sub-menu' exist
   var navigationItem = $('.main-nav-item');
-  navigationItem.each(function () {
-    if ($(this).find('ul.sub-sub-menu-wrapper').length > 0) {
-      $(this).find('div.sub-menu-wrapper').css('column-count', '2');
+  navigationItem.each(function() {
+    if ($(this).find('.sub-sub-menu-wrapper').length > 0) {
+      $(this).find('.sub-menu-wrapper').css('column-count', '2');
     }
   });
 
