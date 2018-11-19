711c785bf2abf4109daa6fc8ca49fe6e03b2246d
Krisztina Elekes <krisztina.n.elekes@gmail.com>
2018-11-19 15:03:23 +0200

    On mobile hide header on scroll down

--- eea/climateadapt/theme/climateadaptv2/src/js/main.js
+++ eea/climateadapt/theme/climateadaptv2/src/js/main.js
@@ -225,6 +225,43 @@ $(document).ready(function() {
     return false;
   });
 
+  // Mobile - hide header on scroll down
+  var didScroll;
+  var lastScrollTop = 0;
+  var delta = 5;
+  var navbarHeight = $('.top-menu').outerHeight();
+
+  $(window).scroll(function(event) {
+    didScroll = true;
+  });
+
+  if (windowWidth <= 800) {
+    setInterval(function() {
+      if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+      }
+    }, 250);
+  }
+
+  function hasScrolled() {
+    var st = $(this).scrollTop();
+
+    if (Math.abs(lastScrollTop - st) <= delta)
+      return;
+
+    if (st > lastScrollTop && st > navbarHeight) {
+      // Scroll down
+      $('.top-menu').removeClass('nav-down').addClass('nav-up');
+    } else {
+      // Scroll up
+      if (st + $(window).height() < $(document).height()) {
+        $('.top-menu').removeClass('nav-up').addClass('nav-down');
+      }
+    }
+    lastScrollTop = st;
+  }
+
 
   // Top menu login
   $('#user-name').click(function(e) {
