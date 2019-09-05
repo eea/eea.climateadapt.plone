c35f6cda88ffdb40a5213c262e85f80658b7a24a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2019-09-05 15:03:30 +0300

    Adjust labels in countries map

--- eea/climateadapt/theme/climateadaptv2/static/countries/countries-tile.js
+++ eea/climateadapt/theme/climateadaptv2/static/countries/countries-tile.js
@@ -50,7 +50,7 @@ $(document).ready(function() {
           drawMap(width);
         }
         drawMap(width);
-      };
+      }
     }
   })
 
@@ -584,7 +584,7 @@ function getIEVersion() {
     return parseInt(sAgent.substring(Idx+ 5, sAgent.indexOf(".", Idx)));
 
   // If IE 11 then look for Updated user agent string.
-  else if (!!navigator.userAgent.match(/Trident\/7\./))
+  else if (navigator.userAgent.match(/Trident\/7\./))
     return 11;
 
   else
