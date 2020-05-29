9f993ff3357bc4962475eab309ead5231f6e7189
David Silaghi <david.silaghi@eaudeweb.ro>
2020-05-29 13:00:31 +0300

    Filter news and events by Observatory keyword

--- eea/climateadapt/browser/health.py
+++ eea/climateadapt/browser/health.py
@@ -1,8 +1,8 @@
 # from functools import partial
+import datetime
 from collections import defaultdict, namedtuple
 
 import DateTime
-import datetime
 import plone.api as api
 from Products.Five.browser import BrowserView
 
@@ -20,30 +20,32 @@ class HealthHomepageItems(BrowserView):
         results = []
 
         now = DateTime.DateTime() - 1
-        date_range_query = { 'query':(now,), 'range': 'min'}
+        date_range_query = {'query': (now,), 'range': 'min'}
 
         portal_catalog = api.portal.get_tool('portal_catalog')
         items = portal_catalog.queryCatalog({
-                "portal_type":"Event",
-                "start" : date_range_query,
-                "sort_limit":"3",
-                "review_state":"published",
-                "sort_on":'start'
-            })
+            "portal_type": "Event",
+            "start": date_range_query,
+            "sort_limit": "3",
+            "review_state": "published",
+            "sort_on": 'start',
+            "Subject": ("Observatory", ),
+        })
 
         for item in items:
             days = (datetime.datetime.strptime(
-                        item.getObject().start.strftime('%d.%m.%Y'),'%d.%m.%Y')
-                        -datetime.datetime.now()
-                    ).days
+                item.getObject().start.strftime('%d.%m.%Y'), '%d.%m.%Y')
+                - datetime.datetime.now()
+            ).days
 
             size = self.days_elapsed_mapping(days)
             object = {
                 'title': item.Title,
                 'size': size,
                 'url': item.getURL(),
-                'date': item.getObject().start.strftime('%d.%m.%Y')
-                }
+                'date': item.getObject().start.strftime('%d.%m.%Y'),
+                "Subject": ("Observatory", ),
+            }
             results.append(object)
 
         return results
@@ -52,26 +54,26 @@ class HealthHomepageItems(BrowserView):
         results = []
         portal_catalog = api.portal.get_tool('portal_catalog')
         items = portal_catalog.queryCatalog({
-                "portal_type":"News Item",
-                "sort_limit":"3",
-                "review_state":"published",
-                "sort_on":'created',
-                "sort_order":'descending'
-            })
+            "portal_type": "News Item",
+            "sort_limit": "3",
+            "review_state": "published",
+            "sort_on": 'created',
+            "sort_order": 'descending'
+        })
 
         for item in items:
             days = (datetime.datetime.strptime(
-                    item.getObject().created().strftime('%d.%m.%Y'),'%d.%m.%Y')
-                    -datetime.datetime.now()
-                ).days
+                    item.getObject().created().strftime('%d.%m.%Y'), '%d.%m.%Y')
+                    - datetime.datetime.now()
+                    ).days
 
-            size = self.days_elapsed_mapping(days*-1)
+            size = self.days_elapsed_mapping(days * -1)
             object = {
                 'title': item.Title,
                 'size': size,
                 'url': item.getURL(),
                 'date': item.getObject().created().strftime('%d.%m.%Y')
-                }
+            }
             results.append(object)
 
         return results
