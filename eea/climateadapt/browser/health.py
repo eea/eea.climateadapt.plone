2f6ff14ad85ed6e2427b724130eafdc72de86f6e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-01-07 14:19:16 +0200

    Code formatting

--- eea/climateadapt/browser/health.py
+++ eea/climateadapt/browser/health.py
@@ -1,5 +1,5 @@
 import datetime
-from collections import defaultdict
+
 from zope.component.hooks import getSite
 
 import DateTime
@@ -8,44 +8,47 @@ from Products.Five.browser import BrowserView
 
 
 class HealthHomepageItems(BrowserView):
-
     def days_elapsed_mapping(self, p):
-        mapping = [(80, 'big'), (90, 'med')]
+        mapping = [(80, "big"), (90, "med")]
         for check, value in mapping:
             if p <= check:
                 return value
-        return 'sma'
+        return "sma"
 
     def upcomingEvents(self):
         results = []
 
         now = DateTime.DateTime() - 1
-        date_range_query = {'query': (now,), 'range': 'min'}
+        date_range_query = {"query": (now,), "range": "min"}
 
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        items = portal_catalog.queryCatalog({
-            "portal_type": "Event",
-            "start": date_range_query,
-            "sort_limit": "2",
-            "review_state": "published",
-            "sort_on": 'start',
-            "Subject": ("Health Observatory", ),
-        })
+        portal_catalog = api.portal.get_tool("portal_catalog")
+        items = portal_catalog.queryCatalog(
+            {
+                "portal_type": "Event",
+                "start": date_range_query,
+                "sort_limit": "2",
+                "review_state": "published",
+                "sort_on": "start",
+                "Subject": ("Health Observatory",),
+            }
+        )
 
         for item in items:
             # TODO: don't convert DateTime to datetime and back
-            days = (datetime.datetime.strptime(
-                item.getObject().start.strftime('%d.%m.%Y'), '%d.%m.%Y')
+            days = (
+                datetime.datetime.strptime(
+                    item.getObject().start.strftime("%d.%m.%Y"), "%d.%m.%Y"
+                )
                 - datetime.datetime.now()
             ).days
 
             size = self.days_elapsed_mapping(days)
             info = {
-                'title': item.Title,
-                'size': size,
-                'url': item.getURL(),
-                'date': item.getObject().start.strftime('%d.%m.%Y'),
-                "Subject": ("Health Observatory", ),
+                "title": item.Title,
+                "size": size,
+                "url": item.getURL(),
+                "date": item.getObject().start.strftime("%d.%m.%Y"),
+                "Subject": ("Health Observatory",),
             }
             results.append(info)
 
@@ -53,30 +56,32 @@ class HealthHomepageItems(BrowserView):
 
     def latestNews(self):
         results = []
-        portal_catalog = api.portal.get_tool('portal_catalog')
-        items = portal_catalog.queryCatalog({
-            "portal_type": "News Item",
-            "sort_limit": "2",
-            "review_state": "published",
-            "sort_on": 'created',
-            "sort_order": 'descending',
-            "Subject": ("Health Observatory", ),
-        })
+        portal_catalog = api.portal.get_tool("portal_catalog")
+        items = portal_catalog.queryCatalog(
+            {
+                "portal_type": "News Item",
+                "sort_limit": "2",
+                "review_state": "published",
+                "sort_on": "created",
+                "sort_order": "descending",
+                "Subject": ("Health Observatory",),
+            }
+        )
 
         strptime = datetime.datetime.strptime
 
         for item in items:
             # TODO: don't convert DateTime to datetime and back
-            created = item.getObject().created().strftime('%d.%m.%Y')
+            created = item.getObject().created().strftime("%d.%m.%Y")
             now = datetime.datetime.now()
-            days = (strptime(created, '%d.%m.%Y') - now).days
+            days = (strptime(created, "%d.%m.%Y") - now).days
 
             size = self.days_elapsed_mapping(days * -1)
             info = {
-                'title': item.Title,
-                'size': size,
-                'url': item.getURL(),
-                'date': item.getObject().created().strftime('%d.%m.%Y')
+                "title": item.Title,
+                "size": size,
+                "url": item.getURL(),
+                "date": item.getObject().created().strftime("%d.%m.%Y"),
             }
             results.append(info)
 
@@ -85,11 +90,11 @@ class HealthHomepageItems(BrowserView):
     @property
     def more_news(self):
         site = getSite()
-        url = site['news-archive'].absolute_url()
+        url = site["news-archive"].absolute_url()
         return [url, "More news"]
 
     @property
     def more_events(self):
         site = getSite()
-        url = site['more-events'].absolute_url()
+        url = site["more-events"].absolute_url()
         return [url, "More events"]
