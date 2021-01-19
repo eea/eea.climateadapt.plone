65698acabca509497d3d935d682e34ad70faa4fc
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-01-19 12:08:54 +0200

    Add aq traverser

---
+++ eea/climateadapt/traversal.py
@@ -0,0 +1,24 @@
+from zope.component import adapter, getUtility
+from zope.interface import implementer
+from zope.publisher.interfaces.browser import IBrowserRequest
+from zope.traversing.interfaces import ITraversable
+
+from plone.app.layout.navigation.interfaces import INavigationRoot
+from plone.contentrules.engine.interfaces import IRuleStorage
+
+
+@adapter(INavigationRoot, IBrowserRequest)
+@implementer(ITraversable)
+class AcquisitionNamespace(object):
+    """Used to traverse to a content.
+
+    Traversing to portal/++aq++path/to/something, acquisition-wrapped.
+    """
+
+    def __init__(self, context, request=None):
+        self.context = context
+        self.request = request
+
+    def traverse(self, name, ignore):
+        base = self.context.restrictedTraverse(name).aq_base
+        return base.__of__(self.context)
