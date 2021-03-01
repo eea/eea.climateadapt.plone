1aa71c76077a9f27e22fb47b1125dd1fdc37b4ea
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-03-01 18:49:23 +0200

    Fix #129328

--- eea/climateadapt/browser/__init__.py
+++ eea/climateadapt/browser/__init__.py
@@ -2,11 +2,9 @@ import json
 import logging
 from collections import namedtuple
 
-from collective.cover.browser.cover import Standard
-from zope.component import queryAdapter
-
 from AccessControl import getSecurityManager
 from Acquisition import aq_inner
+from collective.cover.browser.cover import Standard
 from eea.climateadapt.vocabulary import (BIOREGIONS, SUBNATIONAL_REGIONS,
                                          ace_countries_dict)
 from eea.geotags.behavior.geotags import ISingleGeoTag
@@ -21,21 +19,21 @@ from plone.memoize import view
 from Products.CMFCore.utils import getToolByName
 from Products.Five.browser import BrowserView
 from zExceptions import NotFound
+from zope.component import queryAdapter
 
-logger = logging.getLogger('eea.climateadapt')
+logger = logging.getLogger("eea.climateadapt")
 
-ReviewInfo = namedtuple('ReviewInfo', ['creator', 'reviewer'])
+ReviewInfo = namedtuple("ReviewInfo", ["creator", "reviewer"])
 
 
 class AceViewApi(object):
-
     def geotag(self):
         tag = queryAdapter(self.context, ISingleGeoTag)
 
         return tag
 
     def get_review_info(self):
-        """ Return review information.
+        """Return review information.
 
         See https://taskman.eionet.europa.eu/issues/90376
         """
@@ -43,47 +41,47 @@ class AceViewApi(object):
         creator = self.context.Creator()
         reviewer = None
 
-        if creator is 'tibi':
+        if creator == "tibi":
             creator = None
 
         wh = self.context.workflow_history
-        wf = wh.get('cca_items_workflow', None)
+        wf = wh.get("cca_items_workflow", None)
 
         if not wf:
             return None
 
         for reviewer_metadata in wf:
-            if reviewer_metadata['review_state'] == 'published':
-                reviewer = reviewer_metadata['actor']
+            if reviewer_metadata["review_state"] == "published":
+                reviewer = reviewer_metadata["actor"]
 
         if creator:
             member = get(creator)
 
             if member:
-                creator = member.getProperty('fullname') or creator
+                creator = member.getProperty("fullname") or creator
 
         if reviewer:
             member = get(reviewer)
 
             if member:
-                reviewer = member.getProperty('fullname') or reviewer
+                reviewer = member.getProperty("fullname") or reviewer
 
         return ReviewInfo(creator, reviewer)
 
     def get_date_updated(self):
         wh = self.context.workflow_history
-        wf = wh.get('cca_items_workflow', None)
+        wf = wh.get("cca_items_workflow", None)
 
-        response = {};
-        response['cadapt_last_modified'] = self.context.modified()
-        response['cadapt_published'] = self.context.effective()
+        response = {}
+        response["cadapt_last_modified"] = self.context.modified()
+        response["cadapt_published"] = self.context.effective()
 
         if not wf:
             return response
 
         for metadata in wf:
-            if metadata['action'] == 'publish':
-                response['cadapt_published'] = metadata['time']
+            if metadata["action"] == "publish":
+                response["cadapt_published"] = metadata["time"]
 
         return response
 
@@ -96,9 +94,8 @@ class AceViewApi(object):
     def html2text(self, html):
         if not isinstance(html, basestring):
             return u""
-        portal_transforms = api.portal.get_tool(name='portal_transforms')
-        data = portal_transforms.convertTo('text/plain',
-                                           html, mimetype='text/html')
+        portal_transforms = api.portal.get_tool(name="portal_transforms")
+        data = portal_transforms.convertTo("text/plain", html, mimetype="text/html")
         text = data.getData()
 
         return text.strip()
@@ -107,13 +104,13 @@ class AceViewApi(object):
         if not text:
             return
 
-        if text.startswith('/') or text.startswith('http'):
+        if text.startswith("/") or text.startswith("http"):
             return text
 
         return "http://" + text
 
-    def get_websites(self, field_name = 'websites'):
-        """ This returns a list of websites. Because of BBB, we need to treat
+    def get_websites(self, field_name="websites"):
+        """This returns a list of websites. Because of BBB, we need to treat
         them in various ways
         """
 
@@ -127,16 +124,19 @@ class AceViewApi(object):
             return result
 
         for link in websites:
-            result.append({'url': self.linkify(link), 'title': link})
+            result.append({"url": self.linkify(link), "title": link})
 
         return result
 
     def get_files(self):
-        files = self.context.contentValues({'portal_type': 'File'})
+        files = self.context.contentValues({"portal_type": "File"})
 
         for r in self.context.relatedItems:
-            if r.to_object.portal_type in ['File', 'Image']:
-                files.append(r.to_object)
+            obj = r.to_object
+            if obj is None:
+                continue
+            if obj.portal_type in ["File", "Image"]:
+                files.append(obj)
 
         # return [r.to_object for r in self.context.relatedItems] \
         #     + self.context.contentValues({'portal_type': 'File'})
@@ -153,14 +153,18 @@ class AceViewApi(object):
         #     return value + u":<br/>"
 
     def _render_geochar_macrotrans(self, value):
-        tpl = (u"<div class='sidebar_bold'>"
-               u"<h5>Macro-Transnational region:</h5><p>{0}</p></div>")
+        tpl = (
+            u"<div class='sidebar_bold'>"
+            u"<h5>Macro-Transnational region:</h5><p>{0}</p></div>"
+        )
 
         return tpl.format(u", ".join([BIOREGIONS[x] for x in value]))
 
     def _render_geochar_biotrans(self, value):
-        tpl = (u"<div class='sidebar_bold'>"
-               u"<h5>Biogeographical regions:</h5><p>{0}</p></div>")
+        tpl = (
+            u"<div class='sidebar_bold'>"
+            u"<h5>Biogeographical regions:</h5><p>{0}</p></div>"
+        )
 
         return tpl.format(u", ".join([BIOREGIONS.get(x, x) for x in value]))
 
@@ -170,14 +174,13 @@ class AceViewApi(object):
         return tpl.format(u", ".join(self.get_countries(value)))
 
     def _render_geochar_subnational(self, value):
-        tpl = (u"<div class='sidebar_bold'>"
-               u"<h5>Sub Nationals:</h5><p>{0}</p></div>")
+        tpl = u"<div class='sidebar_bold'>" u"<h5>Sub Nationals:</h5><p>{0}</p></div>"
         # a list like: ['SUBN_Marche__IT_']
 
         out = []
 
         for line in value:
-            line = line.encode('utf-8')
+            line = line.encode("utf-8")
 
             if line in SUBNATIONAL_REGIONS:
                 out.append(SUBNATIONAL_REGIONS[line])
@@ -186,7 +189,7 @@ class AceViewApi(object):
             else:
                 logger.error("Subnational region not found: %s", line)
 
-        text = u", ".join([x.decode('utf-8') for x in out])
+        text = u", ".join([x.decode("utf-8") for x in out])
 
         return tpl.format(text)
 
@@ -196,8 +199,9 @@ class AceViewApi(object):
         if isinstance(value, (list, tuple)):
             text = u", ".join(value)
 
-        return (u"<div class='sidebar_bold'>"
-                u"<h5>City:</h5><p>{0}</p></div>".format(text))
+        return u"<div class='sidebar_bold'>" u"<h5>City:</h5><p>{0}</p></div>".format(
+            text
+        )
 
     @view.memoize
     def render_geochar(self, value):
@@ -215,29 +219,35 @@ class AceViewApi(object):
         value = json.loads(value)
 
         out = []
-        order = ['element', 'macrotrans', 'biotrans',
-                 'countries', 'subnational', 'city']
+        order = [
+            "element",
+            "macrotrans",
+            "biotrans",
+            "countries",
+            "subnational",
+            "city",
+        ]
 
         """ At observatory partner page for organisations remove
         a few categorizations
         """
 
         if (
-            'observatory_page' in self.request.form
+            "observatory_page" in self.request.form
             and self.context.include_in_observatory
-            and self.context.search_type == 'ORGANISATION'
+            and self.context.search_type == "ORGANISATION"
         ):
-            if 'macrotrans' in order:
-                order.remove('biotrans')
-            if 'countries' in order:
-                order.remove('countries')
-            if 'subnational' in order:
-                order.remove('subnational')
-            if 'city' in order:
-                order.remove('city')
+            if "macrotrans" in order:
+                order.remove("biotrans")
+            if "countries" in order:
+                order.remove("countries")
+            if "subnational" in order:
+                order.remove("subnational")
+            if "city" in order:
+                order.remove("city")
 
         for key in order:
-            element = value['geoElements'].get(key)
+            element = value["geoElements"].get(key)
 
             if element:
                 renderer = getattr(self, "_render_geochar_" + key)
@@ -246,28 +256,30 @@ class AceViewApi(object):
         return u" ".join(out)
 
     def link_to_original(self):
-        """ Returns link to original object, to allow easy comparison
-        """
+        """Returns link to original object, to allow easy comparison"""
         site = "http://climate-adapt-old.eea.europa.eu"
 
-        if hasattr(self.context, '_aceitem_id'):
-            return ("{0}/viewaceitem?aceitem_id={1}".format(
-                site, self.context._aceitem_id))
+        if hasattr(self.context, "_aceitem_id"):
+            return "{0}/viewaceitem?aceitem_id={1}".format(
+                site, self.context._aceitem_id
+            )
 
-        if hasattr(self.context, '_acemeasure_id'):
-            return ("{0}/viewmeasure?ace_measure_id={1}".format(
-                site, self.context._acemeasure_id))
+        if hasattr(self.context, "_acemeasure_id"):
+            return "{0}/viewmeasure?ace_measure_id={1}".format(
+                site, self.context._acemeasure_id
+            )
 
-        if hasattr(self.context, '_aceproject_id'):
-            return (
-                "{0}/projects1?ace_project_id={1}".format(
-                    site, self.context._aceproject_id))
+        if hasattr(self.context, "_aceproject_id"):
+            return "{0}/projects1?ace_project_id={1}".format(
+                site, self.context._aceproject_id
+            )
 
     def get_countries(self, country_list):
         return [ace_countries_dict.get(x, x) for x in country_list]
 
     def type_label(self):
         from eea.climateadapt.vocabulary import _datatypes
+
         d = dict(_datatypes)
 
         return d[self.context.search_type]
@@ -280,21 +292,23 @@ class AceViewApi(object):
             return ""
 
         from eea.climateadapt.vocabulary import _governance
+
         d = dict(_governance)
 
         return [d.get(b) for b in self.context.governance_level]
 
     def check_user_role(self):
-        mt = getToolByName(self.context, 'portal_membership')
-        groups_tool = getToolByName(self.context, 'portal_groups')
+        mt = getToolByName(self.context, "portal_membership")
+        groups_tool = getToolByName(self.context, "portal_groups")
         user = mt.getAuthenticatedMember()
 
-        user_groups = [group.id
-                       for group in groups_tool.getGroupsByUserId(user.id)]
-        to_check = ['extranet-cca-reviewers',
-                    'Administrators',
-                    'extranet-cca-powerusers',
-                    'extranet-cca-managers']
+        user_groups = [group.id for group in groups_tool.getGroupsByUserId(user.id)]
+        to_check = [
+            "extranet-cca-reviewers",
+            "Administrators",
+            "extranet-cca-powerusers",
+            "extranet-cca-managers",
+        ]
         check_roles = [group for group in user_groups if group in to_check]
 
         if len(check_roles) > 0:
@@ -304,12 +318,11 @@ class AceViewApi(object):
 
 
 class ViewAceItem(BrowserView):
-    """ Redirection view for /viewaceitem?aceitem_id=..."
-    """
+    """Redirection view for /viewaceitem?aceitem_id=..." """
 
     def __call__(self, REQUEST):
 
-        aceitem_id = REQUEST.get('aceitem_id')
+        aceitem_id = REQUEST.get("aceitem_id")
 
         if aceitem_id:
             try:
@@ -319,39 +332,37 @@ class ViewAceItem(BrowserView):
         else:
             raise NotFound
 
-        return redirect(self.context, REQUEST, 'aceitem_id', aceitem_id)
+        return redirect(self.context, REQUEST, "aceitem_id", aceitem_id)
 
 
 class ViewAceMeasure(BrowserView):
-    """ Redirection view for /viewacemeasure?ace_measure_id=..."
-    """
+    """Redirection view for /viewacemeasure?ace_measure_id=..." """
 
     def __call__(self, REQUEST):
 
-        acemeasure_id = REQUEST.get('ace_measure_id')
+        acemeasure_id = REQUEST.get("ace_measure_id")
 
         if (not acemeasure_id) or (not acemeasure_id.isdigit()):
             raise NotFound
 
         acemeasure_id = int(acemeasure_id)
 
-        return redirect(self.context, REQUEST, 'acemeasure_id', acemeasure_id)
+        return redirect(self.context, REQUEST, "acemeasure_id", acemeasure_id)
 
 
 class ViewAceProject(BrowserView):
-    """ Redirection view for /project1?ace_project_id=..."
-    """
+    """Redirection view for /project1?ace_project_id=..." """
 
     def __call__(self, REQUEST):
 
-        aceproject_id = REQUEST.get('ace_project_id')
+        aceproject_id = REQUEST.get("ace_project_id")
 
         if not aceproject_id or not aceproject_id.isdigit():
             raise NotFound
 
         aceproject_id = int(aceproject_id)
 
-        return redirect(self.context, REQUEST, 'aceproject_id', aceproject_id)
+        return redirect(self.context, REQUEST, "aceproject_id", aceproject_id)
 
 
 def redirect(site, REQUEST, key, itemid):
@@ -366,20 +377,17 @@ def redirect(site, REQUEST, key, itemid):
 
 
 class CoverNoTitleView(Standard):
-    """ A template for covers that doesn't include the title
-    """
+    """A template for covers that doesn't include the title"""
 
     def __call__(self):
         return self.index()
 
 
 class IterateControl(Control):
-    """ Better behaviour for plone.app.iterate
-    """
+    """Better behaviour for plone.app.iterate"""
 
     def is_checkout(self):
-        """ Is this object a checkout? Used by CCA for workflow guards
-        """
+        """Is this object a checkout? Used by CCA for workflow guards"""
         context = aq_inner(self.context)
 
         if not IIterateAware.providedBy(context):
@@ -396,8 +404,7 @@ class IterateControl(Control):
         return False
 
     def checkin_allowed(self):
-        """ Overrided to check for the checkin permission, as it should be normal
-        """
+        """Overrided to check for the checkin permission, as it should be normal"""
 
         context = aq_inner(self.context)
         checkPermission = getSecurityManager().checkPermission
@@ -420,7 +427,7 @@ class IterateControl(Control):
 
         try:
             original = policy.getBaseline()
-        except:
+        except Exception:
             return False
 
         if original is None:
@@ -434,8 +441,7 @@ class IterateControl(Control):
         return True
 
     def checkout_allowed(self):
-        """ Overrided to check for the checkout permission, as it is normal
-        """
+        """Overrided to check for the checkout permission, as it is normal"""
         context = aq_inner(self.context)
 
         if not IIterateAware.providedBy(context):
@@ -468,7 +474,7 @@ class IterateControl(Control):
 
     def cancel_allowed(self):
         """Check to see if the user can cancel the checkout on the given
-           working copy.
+        working copy.
         """
         policy = ICheckinCheckoutPolicy(self.context, None)
 
@@ -479,9 +485,9 @@ class IterateControl(Control):
         if wc is None:
             return False
 
-        has_wc = (wc is not None)
-        is_wc = (self.context.aq_inner.aq_self is wc.aq_inner.aq_self)
+        has_wc = wc is not None
+        is_wc = self.context.aq_inner.aq_self is wc.aq_inner.aq_self
         res = has_wc and is_wc
-        print "Checkout cancel allowed: ", res
+        print("Checkout cancel allowed: ", res)
 
         return res
