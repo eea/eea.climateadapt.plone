3918c795a7ba11719e99356be1e9a94bc3500e9e
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2021-04-23 16:13:42 +0300

    Add listing template

--- eea/climateadapt/scripts/c3s.py
+++ eea/climateadapt/scripts/c3s.py
@@ -12,115 +12,148 @@ This commands accepts various parameter. Look at __main__ to see what it does.
 # TODO: this script is hackish. Maybe optparse would improve feeling.
 """
 
-import argparse
 import json
 import logging
-import os
-import sys
-import urllib2
-import Zope2
-import transaction
 
+import transaction
+import urllib2
+from eea.climateadapt.scripts import get_plone_site
+from plone import api
 from plone.app.textfield.value import RichTextValue
 
-from lxml.etree import Element, SubElement, fromstring, tostring
-from eea.climateadapt.scripts import get_plone_site
-
-logger = logging.getLogger('eea.climateadapt')
+logger = logging.getLogger("eea.climateadapt")
 logging.basicConfig()
 
-SOURCE_URL = 'https://raw.githubusercontent.com/maris-development/c3s-434-portal/static-generator/data/data_consolidated.json'
-HOST = 'climate-adapt.eea.europa.eu'
-PLONE = "/cca"
+SOURCE_URL = (
+    "https://raw.githubusercontent.com/maris-development/c3s-434-portal/"
+    "static-generator/data/data_consolidated.json"
+)
+
 
 def get_source_data():
     response = urllib2.urlopen(SOURCE_URL)
     source_content = response.read()
     return json.loads(source_content)
 
-def save_indicator2(indicator):
-        from plone import api
-        portal = api.portal.get()
 
-def save_object(obj, indicator):
+def update_object(obj, indicator):
     print(obj.title)
-    print('  ->'+obj.c3s_identifier)
-    #if 'C3S_434_021' == obj.c3s_identifier:
+    print("  ->" + obj.c3s_identifier)
+    # if 'C3S_434_021' == obj.c3s_identifier:
     #    import pdb; pdb.set_trace()
-    obj.title = indicator['page_title']
-    obj.long_description = RichTextValue(indicator['description_detail'])
 
-    obj.definition_app = RichTextValue('<h4>'+indicator['indicator_title']+'</h4>'+indicator['description'])
+    obj.title = indicator["page_title"]
+    obj.long_description = RichTextValue(indicator["description_detail"])
 
-    obj.overview_app_toolbox_url = indicator['overview']
-    obj.overview_app_parameters = '{}'
-    if indicator['vars']['overview']:
-        obj.overview_app_parameters = '{workflowParams:' + json.dumps(indicator['vars']['overview']) + '}'
+    obj.definition_app = RichTextValue(
+        "<h4>" + indicator["indicator_title"] + "</h4>" + indicator["description"]
+    )
 
-    obj.details_app_toolbox_url = indicator['detail']
-    obj.details_app_parameters = '{}'
-    if indicator['vars']['detail']:
-        obj.overview_app_parameters = '{workflowParams:' + json.dumps(indicator['vars']['detail']) + '}'
+    obj.overview_app_toolbox_url = indicator["overview"]
+    obj.overview_app_parameters = "{}"
+    if indicator["vars"]["overview"]:
+        obj.overview_app_parameters = (
+            "{workflowParams:" + json.dumps(indicator["vars"]["overview"]) + "}"
+        )
+
+    obj.details_app_toolbox_url = indicator["detail"]
+    obj.details_app_parameters = "{}"
+    if indicator["vars"]["detail"]:
+        obj.overview_app_parameters = (
+            "{workflowParams:" + json.dumps(indicator["vars"]["detail"]) + "}"
+        )
 
     obj.sectors = []
     obj.climate_impacts = []
+
+    state = api.content.get_state(obj=obj, default="Unknown")
+    if state != "published":
+        print("Object not published, publishing", obj)
+        api.content.transition(obj, "publish")
     obj._p_changed = True
 
 
-def save_indicator(indicator):
+def save_indicator(indicator, site, data):
 
-    print('=============================================')
-    #print(type(indicator))
-    #print(indicator.keys())
-    #print(indicator)
+    print("=============================================")
+    # print(type(indicator))
+    # print(indicator.keys())
+    # print(indicator)
 
-    site = get_plone_site()
-    print(indicator['theme'])
+    print(indicator["theme"])
 
     portal_catalog = site.portal_catalog
-    brains = portal_catalog.unrestrictedSearchResults(**{'portal_type': 'eea.climateadapt.c3sindicator', 'c3s_identifier': indicator['identifier']})
+    brains = portal_catalog.unrestrictedSearchResults(
+        **{
+            "portal_type": "eea.climateadapt.c3sindicator",
+            "c3s_identifier": indicator["identifier"],
+        }
+    )
     indicatorFound = False
+
     for brain in brains:
         obj = brain.getObject()
-        #print(brain.getURL())
-        #print('--->'+obj.title)
+        # print(brain.getURL())
+        # print('--->'+obj.title)
         try:
-            if indicator['identifier'] == obj.c3s_identifier:
+            if indicator["identifier"] == obj.c3s_identifier:
                 indicatorFound = True
-                save_object(obj, indicator)
-                transaction.commit()
-                print('  UPDATE OBJECT')
+                update_object(obj, indicator)
+                print("  UPDATE OBJECT")
         except:
             """"""
-            #print("C3S Identifier NOT SET")
+            # print("C3S Identifier NOT SET")
 
     if not indicatorFound:
-        folder_path = 'knowledge/european-climate-data-explorer/'
-        folder_indicator_id = indicator['theme'].lower().replace(' ','-')
+        folder_path = "knowledge/european-climate-data-explorer/"
+        folder_indicator_id = indicator["theme"].lower().replace(" ", "-")
 
         from plone.dexterity.utils import createContentInContainer
 
         folder = site.restrictedTraverse(folder_path)
         if folder_indicator_id not in folder.contentIds():
-            folder_indicator = createContentInContainer(folder, "Folder", title=indicator['theme'])
+            print("Create indicator folder", indicator["theme"])
+            folder_indicator = createContentInContainer(
+                folder, "Folder", title=indicator["theme"]
+            )
+
+            folder_indicator.manage_changeProperties(
+                layout="c3s_simplified_listing_view"
+            )
+            api.content.transition(folder_indicator, "publish")
+            folder_indicator._p_changed
         else:
             folder_indicator = folder[folder_indicator_id]
 
+        obj = createContentInContainer(
+            folder_indicator,
+            "eea.climateadapt.c3sindicator",
+            title=indicator["page_title"],
+        )
 
-        obj = createContentInContainer(folder_indicator, "eea.climateadapt.c3sindicator", title=indicator['page_title'])
-        obj.c3s_identifier = indicator['identifier']
-        save_object(obj, indicator)
-        transaction.commit()
-        print('  INSERT OBJECT')
+        obj.c3s_identifier = indicator["identifier"]
+        update_object(obj, indicator)
 
 
 def main():
+    site = get_plone_site()
     data = get_source_data()
-    for indicator_identifier in data['indicators']:
-        #print(data['indicators'][indicator_identifier])
-        save_indicator(data['indicators'][indicator_identifier])
+    for indicator_identifier in data["indicators"]:
+        # print(data['indicators'][indicator_identifier])
+        save_indicator(data["indicators"][indicator_identifier], site, data)
 
-    print("Total items:" + str(len(data['indicators'])))
+    for theme_id in data["themes"]:
+        theme_folder = site["knowledge"]["european-climate-data-explorer"][theme_id]
+        theme_folder.text = RichTextValue(
+            data["themes"][theme_id]["description"], "text/plain", "text/html"
+        )
+        theme_folder._p_changed = True
+        print("Updated description for", theme_folder)
+
+        # c3s_simplified_listing_view
+    transaction.commit()
+    print("Total items:" + str(len(data["indicators"])))
+
 
 if __name__ == "__main__":
     main()
