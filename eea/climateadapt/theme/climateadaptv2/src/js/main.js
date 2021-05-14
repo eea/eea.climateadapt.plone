71b6f1e88616b377db480e4b4c9f83ef83ed9541
Eugen Tripon <tripon.eugen@gmail.com>
2021-05-14 17:07:42 +0300

    Refs #133981 scrool top

--- eea/climateadapt/theme/climateadaptv2/src/js/main.js
+++ eea/climateadapt/theme/climateadaptv2/src/js/main.js
@@ -1091,6 +1091,20 @@ $(document).ready(function () {
             });
         }, 2000)
     });
+
+    // Scroll to top button
+    var $scrollBtn = $('.scroll-to-top');
+    $(window).scroll(function() {
+      if ($(this).scrollTop() > 500) {
+        $scrollBtn.fadeIn();
+      } else {
+        $scrollBtn.fadeOut();
+      }
+    });
+    $scrollBtn.click(function() {
+      $('html, body').animate({ scrollTop : 0 }, 500);
+      return false;
+    });
 });
 
 // Divide the sub-menu in 2 columns if 'sub-sub-menu' exist
