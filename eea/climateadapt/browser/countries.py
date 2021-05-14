ac560ae4484dc2c1e4c88f99d92ddbcf9b091463
laszlocseh <csehlaszlo91@gmail.com>
2021-05-14 12:41:47 +0300

    Refs #129975 Country profiles refactor code

--- eea/climateadapt/browser/countries.py
+++ eea/climateadapt/browser/countries.py
@@ -1,11 +1,17 @@
 import csv
 import json
 import logging
-
 import lxml.etree
 import lxml.html
+import urllib2
+from datetime import datetime
 from pkg_resources import resource_filename
 from Products.Five.browser import BrowserView
+from Products.Five.browser.pagetemplatefile import ViewPageTemplateFile
+
+from eea.climateadapt.vocabulary import ace_countries
+from plone.intelligenttext.transforms import \
+    convertWebIntelligentPlainTextToHtml as convWebInt
 
 
 def parse_csv(path):
@@ -20,6 +26,32 @@ def parse_csv(path):
 
     return out
 
+
+def setup_discodata(annotations):
+    response = urllib2.urlopen(DISCODATA_URL)
+    data = json.loads(response.read())
+    annotations['discodata'] = {
+        'timestamp': datetime.now(),
+        'data': data
+    }
+    logger.info("RELOAD URL %s", DISCODATA_URL)
+
+    return data
+
+
+def get_discodata(annotations):
+    if 'discodata' not in annotations:
+        return setup_discodata(annotations)
+
+    last_import_date = annotations['discodata']['timestamp']
+
+    if (datetime.now() - last_import_date).total_seconds() > 60 * 2:
+        return setup_discodata(annotations)
+
+    return annotations['discodata']['data']
+
+
+DISCODATA_URL = 'https://discodata.eea.europa.eu/sql?query=select%20*%20from%20%5BNCCAPS%5D.%5Blatest%5D.%5BAdaptation_JSON%5D&p=1&nrOfHits=100'
 
 logger = logging.getLogger("eea.climateadapt")
 
@@ -174,6 +206,8 @@ class CountriesMetadataExtract(BrowserView):
         # if 'ireland' in obj.absolute_url().lower():
         #     import pdb
         #     pdb.set_trace()
+
+        import pdb; pdb.set_trace()
 
         if "index_html" in obj.contentIds():
             cover = obj["index_html"]
@@ -334,3 +368,87 @@ class ContextCountriesView(BrowserView):
         )
 
         return s
+
+
+class CountryProfileData(BrowserView):
+    template = ViewPageTemplateFile("pt/country-profile.pt")
+
+    @property
+    def annotations(self):
+        return self.context.__annotations__
+
+    def convert_web_int(self, text):
+        return convWebInt(text)
+
+    def get_sorted_affected_sectors_data(self):
+        items = self.processed_data['National_Circumstances'].get(
+            'Afected_Sectors', [])
+
+        sorted_items = sorted(
+            items,
+            key=lambda i: (i['SectorTitle'], i['SectorDescribeIfOther'])
+        )
+
+        return sorted_items
+
+    def get_sorted_action_measures_data(self):
+        items = self.processed_data['Strategies_Plans'].get(
+            'Action_Measures', [])
+
+        sorted_items = sorted(
+            items,
+            key=lambda i: (i['KeyTypeMeasure'], i['subKTM'], i['Title'])
+        )
+
+        return sorted_items
+
+    def get_sorted_available_practices_data(self):
+        items = self.processed_data['Cooperation_Experience'].get(
+            'Available_Good_Practices', [])
+
+        sorted_items = sorted(
+            items,
+            key=lambda i: i['Area']
+        )
+
+        return sorted_items
+
+    def __call__(self):
+        country_name = self.context.title
+        country_code = next(
+            (k for k, v in ace_countries if v == country_name), 'Not found'
+        )
+
+        data = get_discodata(self.annotations)
+        # [u'AT', u'BE', u'BG', u'CZ', u'DE', u'DK', u'EE', u'ES', u'FI',
+        # u'GR', u'HR', u'HU', u'IE', u'IT', u'LT', u'LU', u'LV', u'MT',
+        # u'NL', u'PL', u'PT', u'RO', u'SE', u'SI', u'SK', u'TR']
+        orig_data = next((
+            x
+            for x in data['results']
+            if x['countryCode'] == country_code
+        ), {})
+
+        # remove the countryCode as we don't need it
+        processed_data = {
+            k: unicode(v)
+            for k, v in orig_data.items()
+            if k != 'countryCode'
+        }
+
+        # some values are strings, and need to be transformed
+        # into Python objects
+        for k, val in processed_data.items():
+            json_val = json.loads(val)
+            new_value = json_val[k][0]
+
+            processed_data[k] = new_value
+
+        self.processed_data = processed_data
+
+        return self.template(country_data=processed_data,
+                             original_data=orig_data)
+
+
+class CountryProfileDataRaw(CountryProfileData):
+    template = ViewPageTemplateFile("pt/country-profile-raw.pt")
