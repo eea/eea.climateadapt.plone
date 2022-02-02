c459db862e35f6e6b44fc66523406f55964c5d3f
Petchesi-Iulian <iamnotacat12@gmail.com>
2022-02-02 18:51:46 +0200

    More fixes.

--- eea/climateadapt/browser/tilehelpers.py
+++ eea/climateadapt/browser/tilehelpers.py
@@ -294,7 +294,7 @@ class NewsTile(ListingTile):
     def parent(self):
         site = getSite()
 
-        return site['en/news-archive']
+        return site['/en/news-archive']
 
     @view.memoize
     def get_item_date(self, item):
@@ -347,7 +347,7 @@ class EventsTile(ListingTile):
     def parent(self):
         site = getSite()
 
-        return site['en/more-events']
+        return site['/en/more-events']
 
     @view.memoize
     def get_item_date(self, item):
