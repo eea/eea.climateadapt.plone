a773a71c35e367e4d1ea85f1d31dc557050e511b
tripon eugen <tripon.eugen@gmail.com>
2022-02-06 14:50:15 +0200

    Refs #144715 - date expire for archived items

--- eea/climateadapt/contentrules.py
+++ eea/climateadapt/contentrules.py
@@ -1,17 +1,20 @@
 # -*- coding: utf-8 -*-
-
+from plone import api
 from plone.api.portal import get_tool
 import transaction
 from OFS.SimpleItem import SimpleItem
 from plone.app.contentrules import PloneMessageFactory as _
 from plone.app.contentrules.browser.formhelper import NullAddForm
 from plone.contentrules.rule.interfaces import IExecutable, IRuleElementData
+from DateTime import DateTime
 from Products.CMFPlone import utils
 from Products.statusmessages.interfaces import IStatusMessage
 from ZODB.POSException import ConflictError
 from zope.component import adapter
 from zope.interface import Interface, implementer
 
+import logging
+logger = logging.getLogger("eea.climateadapt")
 
 class IReindexAction(Interface):
     """Interface for the configurable aspects of a reindex action. """
@@ -69,3 +72,58 @@ class ReindexAddForm(NullAddForm):
 
     def create(self):
         return ReindexAction()
+
+
+class IObjectDateExpirationAction(Interface):
+    """Interface for the configurable aspects of a archived action. """
+
+
+@implementer(IObjectDateExpirationAction, IRuleElementData)
+class ObjectDateExpirationAction(SimpleItem):
+    """The actual persistent implementation of the action element. """
+
+    element = "eea.climateadapt.ObjectDateExpiration"
+    summary = _(u"Set object expiration date")
+
+
+@adapter(Interface, IObjectDateExpirationAction, Interface)
+@implementer(IExecutable)
+class ObjectDateExpirationActionExecutor(object):
+    """The executor for this action."""
+
+    def __init__(self, context, element, event):
+        self.context = context
+        self.element = element
+        self.event = event
+
+    def __call__(self):
+        obj = self.event.object
+
+        transaction.savepoint()
+
+        catalog = get_tool("portal_catalog")
+
+        try:
+            portal = api.portal.get()
+            state = api.content.get_state(obj)
+            if state == 'published':
+                obj.setExpirationDate(None)
+                obj._p_changed = True
+                obj.reindexObject()
+            if state == 'archived':
+                obj.setExpirationDate(DateTime())
+                obj._p_changed = True
+                obj.reindexObject()
+
+        except Exception as e:
+            self.error(obj, str(e))
+            return False
+
+        return True
+
+
+class ObjectDateExpirationAddForm(NullAddForm):
+    """A reindex action form"""
+
+    def create(self):
+        return ObjectDateExpirationAction()
