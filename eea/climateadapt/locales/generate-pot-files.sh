465eb74cdaeed9530bdd6f4151d92807a2a71d54
tripon eugen <tripon.eugen@gmail.com>
2022-05-17 21:50:56 +0300

    Refs #144341 - transalation files

--- eea/climateadapt/locales/generate-pot-files.sh
+++ eea/climateadapt/locales/generate-pot-files.sh
@@ -16,5 +16,5 @@ i18ndude rebuild-pot \
 i18ndude rebuild-pot \
 	--pot eea.climateadapt.frontpage.pot \
   --create eea.climateadapt.frontpage \
-	--exclude "behaviors profiles tiles *.zcml contentrules.py overrides.py export_portaltypes.pt keywords-admin.pt special-tags-admin.pt" \
+	--exclude "behaviors browser profiles tiles *.zcml contentrules.py overrides.py export_portaltypes.pt keywords-admin.pt special-tags-admin.pt" \
 	../../
