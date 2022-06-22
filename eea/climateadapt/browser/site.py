44a4e9127459215e625fa0f70a71757b3efe86af
tripon eugen <tripon.eugen@gmail.com>
2022-06-22 03:17:37 +0300

    Refs #1488151 - footer links

--- eea/climateadapt/browser/site.py
+++ eea/climateadapt/browser/site.py
@@ -356,6 +356,8 @@ class Navbar(ExternalTemplateHeader):
         return []
 
     def translate_url(self, url):
+        if url == '/':
+            return url
         current_language = get_current_language(self.context, self.request)
         #if 'en' == current_language:
         #    return url
