80e671d0d98281085b6c2814653dcf88a28a132b
GhitaB <ghitabizau27@gmail.com>
2023-01-19 13:28:42 +0200

    Refs #159767 - WIP customize search page to include language filter (monkey patch plone.app.search results).

--- eea/climateadapt/patch.py
+++ eea/climateadapt/patch.py
@@ -2,6 +2,10 @@ from eea.climateadapt.translation.utils import get_current_language
 from plone.app.theming.transform import _Cache
 from zope.globalrequest import getRequest
 from zope.site.hooks import getSite
+from Products.CMFCore.utils import getToolByName
+from Products.CMFPlone.PloneBatch import Batch
+from Products.ZCTextIndex.ParseTree import ParseError
+from plone.app.contentlisting.interfaces import IContentListing
 
 
 def getCache(settings):
@@ -22,3 +26,51 @@ def getCache(settings):
     if cache is None:
         cache = caches[key] = _Cache()
     return cache
+
+
+# https://github.com/plone/plone.app.search/blob/1.1.x/plone/app/search/browser.py#L33
+# Patch the search results to include language filter.
+def results(self, query=None, batch=True, b_size=10, b_start=0):
+    """ Get properly wrapped search results from the catalog.
+    Everything in Plone that performs searches should go through this view.
+    'query' should be a dictionary of catalog parameters.
+    """
+    if query is None:
+        query = {}
+    if batch:
+        query['b_start'] = b_start = int(b_start)
+        query['b_size'] = b_size
+    query = self.filter_query(query)
+
+    # Customization start -----------------------------------------------------
+    languages = ['en', 'fr', 'de', 'it', 'pl', 'es']
+    try:
+        lang = self.request.form.get('language', None)
+        if lang in languages:
+            language = lang
+        else:
+            language = "all"
+    except Exception:
+        language = "all"
+
+    if language == "all":
+        updated_path = "/cca"
+    else:
+        updated_path = "/cca/" + language
+
+    query['path'] = updated_path
+    # Customization end -------------------------------------------------------
+
+    if query is None:
+        results = []
+    else:
+        catalog = getToolByName(self.context, 'portal_catalog')
+        try:
+            results = catalog(**query)
+        except ParseError:
+            return []
+
+    results = IContentListing(results)
+    if batch:
+        results = Batch(results, b_size, b_start)
+    return results
