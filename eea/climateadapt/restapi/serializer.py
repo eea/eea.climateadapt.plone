9c56a38d1552c4e2c0c72042e5ce8128a21b0d47
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-02-09 10:08:26 +0200

    Fix related items

--- eea/climateadapt/restapi/serializer.py
+++ eea/climateadapt/restapi/serializer.py
@@ -1,8 +1,8 @@
-from zope.component import adapter
 from eea.climateadapt.behaviors import IAdaptationOption
-from zope.interface import Interface
-from plone.restapi.serializer.dxcontent import SerializeToJson
 from eea.climateadapt.browser.adaptationoption import find_related_casestudies
+from plone.restapi.serializer.dxcontent import SerializeToJson
+from zope.component import adapter
+from zope.interface import Interface
 
 
 @adapter(IAdaptationOption, Interface)
@@ -13,6 +13,7 @@ class AdaptationOptionSerializer(SerializeToJson):
                 version=None, include_items=True)
         result["zzztestvalue"] = "TEST VALUE"
         item = self.context
+
+        result['related_case_studies'] = find_related_casestudies(item)
         # __import__('pdb').set_trace()
-        # related_case_studies = find_related_casestudies(item)
         return result
