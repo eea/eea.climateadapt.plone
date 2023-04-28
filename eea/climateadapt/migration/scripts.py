f8c721bd8b7766b87c125e08a00fcb1adeff7757
GhitaB <ghitabizau27@gmail.com>
2023-04-28 15:13:03 +0300

    Refs #161595 - Migration script: prepare list of items.

--- eea/climateadapt/migration/scripts.py
+++ eea/climateadapt/migration/scripts.py
@@ -1,15 +1,33 @@
+""" Migrate content to Volto
+"""
 from Products.Five.browser import BrowserView
 from plone.api import portal
+from eea.climateadapt.scripts import get_plone_site
+from eea.climateadapt.translation.admin import get_all_objs
+import logging
+
+logger = logging.getLogger('eea.climateadapt')
 
 
 def _migrate_to_volto(site):
     """ #161595 migration script for Plone 4 to Volto content
     """
-    print "WIP migration"
+    logger.info("--- START MIGRATION -----------------------------")
+    logger.info("Get the list of items ordered by levels...")
+    brains = get_all_objs(site)
+    for brain in brains:
+        obj = brain.getObject()
+        logger.info("Migrating %s" % obj.absolute_url())
+
+        # TODO migration
+
+    logger.info("--- Done ----------------------------------------")
     return "Done"
 
 
 def migrate_to_volto(site=None):
+    """ bin/standalone run bin/migrate_to_volto
+    """
     if site is None:
         site = get_plone_site()
     _migrate_to_volto(site)
