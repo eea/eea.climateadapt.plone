d8e8031317e2955128457c9eccbeee2bf4731af6
GhitaB <ghitabizau27@gmail.com>
2023-05-17 16:57:01 +0300

    Refs #252940 - In observatory indicators list show only indicators having publication date set.

--- eea/climateadapt/browser/observatory_indicators.py
+++ eea/climateadapt/browser/observatory_indicators.py
@@ -40,11 +40,11 @@ class ObservatoryIndicators(BrowserView, TranslationUtilsMixin):
         catalog = get_tool("portal_catalog")
         origin_website = []
         search_params = {
-                        "path": "/cca/"+self.current_lang,
-                        "portal_type": ["eea.climateadapt.indicator","eea.climateadapt.c3sindicator"],
-                        "include_in_observatory":"True",
-                        "review_state": "published"
-                    }
+            "path": "/cca/"+self.current_lang,
+            "portal_type": ["eea.climateadapt.indicator", "eea.climateadapt.c3sindicator"],
+            "include_in_observatory": "True",
+            "review_state": "published"
+        }
         brains = catalog.searchResults(search_params)
         for brain in brains:
             obj = brain.getObject()
@@ -53,15 +53,15 @@ class ObservatoryIndicators(BrowserView, TranslationUtilsMixin):
                     if origin_name not in origin_website:
                         origin_website.append(origin_name)
         origin_website.sort()
-        return [[v,self.map_origin_wesite(v)] for v in origin_website]
+        return [[v, self.map_origin_wesite(v)] for v in origin_website]
 
     def get_search_params(self):
         search_params = {
-                        "path": "/cca/"+self.current_lang,
-                        "portal_type": ["eea.climateadapt.indicator","eea.climateadapt.c3sindicator"],
-                        "include_in_observatory":"True",
-                        "review_state": "published"
-                    }
+            "path": "/cca/"+self.current_lang,
+            "portal_type": ["eea.climateadapt.indicator", "eea.climateadapt.c3sindicator"],
+            "include_in_observatory": "True",
+            "review_state": "published"
+        }
         selected_origin = self.get_selected_origin_websites()
         selected_search = self.get_selected_search()
         if selected_search:
@@ -74,14 +74,15 @@ class ObservatoryIndicators(BrowserView, TranslationUtilsMixin):
         catalog = get_tool("portal_catalog")
         items = []
         health_impacts = {
-            'Heat':{'value':0,'icon':'fa fa-area-chart', 'print':self.get_i18n_for_text('Heat')},
-            'Droughts and floods':{'value':0,'icon':'fa fa-compass', 'print':self.get_i18n_for_text('Droughts and floods')},
-            'Climate-sensitive diseases':{'value':0,'icon':'fa fa-info-circle', 'print':self.get_i18n_for_text('Climate-sensitive diseases')},
-            'Air pollution and aero-allergens':{'value':0,'icon':'fa fa-file-video-o', 'print':self.get_i18n_for_text('Air pollution and aero-allergens')},
-            'Wildfires':{'value':0,'icon':'fa fa-wrench', 'print':self.get_i18n_for_text('Wildfires')}
+            'Heat': {'value': 0, 'icon': 'fa fa-area-chart', 'print': self.get_i18n_for_text('Heat')},
+            'Droughts and floods': {'value': 0, 'icon': 'fa fa-compass', 'print': self.get_i18n_for_text('Droughts and floods')},
+            'Climate-sensitive diseases': {'value': 0, 'icon': 'fa fa-info-circle', 'print': self.get_i18n_for_text('Climate-sensitive diseases')},
+            'Air pollution and aero-allergens': {'value': 0, 'icon': 'fa fa-file-video-o', 'print': self.get_i18n_for_text('Air pollution and aero-allergens')},
+            'Wildfires': {'value': 0, 'icon': 'fa fa-wrench', 'print': self.get_i18n_for_text('Wildfires')}
         }
         search_params = self.get_search_params()
-        brains = catalog.searchResults(search_params, sort_on='sortable_title', sort_order='ascending')
+        brains = catalog.searchResults(
+            search_params, sort_on='sortable_title', sort_order='ascending')
         for brain in brains:
             obj = brain.getObject()
             origin_website = ''
@@ -89,14 +90,16 @@ class ObservatoryIndicators(BrowserView, TranslationUtilsMixin):
                 origin_website = ', '.join(obj.origin_website)
             for key in health_impacts:
                 if key in obj.health_impacts:
-                    health_impacts[key]['value']+=1
-            items.append({
+                    health_impacts[key]['value'] += 1
+
+            if obj.publication_date is not None:
+                items.append({
                     "title": obj.title,
                     "id": brain.UID,
                     "url": brain.getURL(),
                     "origin_websites": self.map_origin_wesite(origin_website),
-                    "health_impacts_list": ' '.join([impact.lower().replace(' ','_') for impact in obj.health_impacts]),
+                    "health_impacts_list": ' '.join([impact.lower().replace(' ', '_') for impact in obj.health_impacts]),
                     "year": obj.publication_date.year
                 })
 
-        return {'items':items,'health_impacts':health_impacts}
+        return {'items': items, 'health_impacts': health_impacts}
