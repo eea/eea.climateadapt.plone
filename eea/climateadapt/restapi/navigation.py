231635def17dbc2c10abc4beed24f42d6ab48612
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-23 15:03:25 +0300

    Also add navigation service

--- eea/climateadapt/restapi/navigation.py
+++ eea/climateadapt/restapi/navigation.py
@@ -1,15 +1,57 @@
+from plone.restapi.interfaces import IExpandableElement, IPloneRestapiLayer
+from plone.restapi.services.navigation.get import Navigation as BaseNavigation
+from plone.restapi.services.navigation.get import \
+    NavigationGet as BaseNavigationGet
 from Products.CMFPlone.browser.navigation import \
     CatalogNavigationTabs as BaseCatalogNavigationTabs
+from zope.component import adapter
+from zope.interface import Interface, implementer
+
+
+# if '/mission' not in self.context.absolute_url(relative=True):
+def is_outside_mission(context):
+    bits = context.getPhysicalPath()
+    if len(bits) > 3 and bits[3] == 'mission':
+        return False
+
+    return True
 
 
 class CatalogNavigationTabs(BaseCatalogNavigationTabs):
     def _getNavQuery(self):
         query = super(CatalogNavigationTabs, self)._getNavQuery()
 
-        bits = self.context.getPhysicalPath()
-        if len(bits) > 3 and bits[3] == 'mission':
+        if not is_outside_mission(self.context):
             return query
-        # if '/mission' not in self.context.absolute_url(relative=True):
+
         query['show_in_top_level'] = True
 
         return query
+
+
+@implementer(IExpandableElement)
+@adapter(Interface, IPloneRestapiLayer)
+class Navigation(BaseNavigation):
+    def __call__(self, expand=False):
+        if self.request.form.get("expand.navigation.depth", False):
+            # self.depth = int(self.request.form["expand.navigation.depth"])
+
+            if is_outside_mission(self.context):
+                self.depth = 1
+            else:
+                self.depth = int(self.request.form["expand.navigation.depth"])
+        else:
+            self.depth = 1
+
+        result = {"navigation": {"@id": self.context.absolute_url() + "/@navigation"}}
+        if not expand:
+            return result
+
+        result["navigation"]["items"] = self.build_tree(self.navtree_path)
+        return result
+
+
+class NavigationGet(BaseNavigationGet):
+    def reply(self):
+        navigation = Navigation(self.context, self.request)
+        return navigation(expand=True)["navigation"]
