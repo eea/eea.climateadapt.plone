c7073a36f5eb54e9388fce588096d41537c6ab98
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-23 20:40:20 +0300

    Code cleanup

--- eea/climateadapt/browser/landing_page.py
+++ eea/climateadapt/browser/landing_page.py
@@ -27,43 +27,6 @@ SEARCH_TYPES_ICONS = [
 ]
 
 
-# filters[0][field]=issued.date&
-# filters[0][type]=any&
-# filters[0][values][0]=Last 5 years&
-# filters[1][field]=language&
-# filters[1][type]=any&
-# filters[1][values][0]=en&
-# filters[2][field]=items_count_language&
-# filters[2][values][0]=n_1_n&
-# filters[2][type]=all&
-# filters[3][field]=objectProvides&
-# filters[3][values][0]=Research and knowledge project&
-# filters[3][type]=any
-
-
-# filters[1][field]=sectors&
-# filters[1][type]=any&
-# filters[1][values][0]=Urban
-
-# filters[0][values][0]=Guidance&
-# filters[0][type]=any&
-# filters[0][field]=objectProvides&
-
-# filters%5B0%5D%5Bfield%5D=issued.date&filters%5B0%5D%5Btype%5D=any&filters%5B0%5D%5Bvalues%5D%5B0%5D=Last%205%20years&filters%5B1%5D%5Bfield%5D=language&filters%5B1%5D%5Btype%5D=any&filters%5B1%5D%5Bvalues%5D%5B0%5D=en&filters%5B2%5D%5Bfield%5D=items_count_language&filters%5B2%5D%5Bvalues%5D%5B0%5D=n_1_n&filters%5B2%5D%5Btype%5D=all&filters%5B3%5D%5Bfield%5D=cca_adaptation_sectors.keyword&filters%5B3%5D%5Bvalues%5D%5B0%5D=Health&filters%5B3%5D%5Btype%5D=any
-
-# filters[0][field]=issued.date&
-# filters[0][type]=any&
-# filters[0][values][0]=Last 5 years&
-# filters[1][field]=language&
-# filters[1][type]=any&
-# filters[1][values][0]=en&
-# filters[2][field]=items_count_language&
-# filters[2][values][0]=n_1_n&
-# filters[2][type]=all&
-# filters[3][field]=cca_adaptation_sectors.keyword&
-# filters[3][values][0]=Health&
-# filters[3][type]=any
-
 def filters_to_query(args):
     res = []
     for i, (name, val) in enumerate(args):
@@ -130,30 +93,6 @@ class Forest(BrowserView, TranslationUtilsMixin):
         link = "/{0}/data-and-downloads/?{1}".format(self.current_lang, query)
 
         return link
-
-        # t = {
-        #     u"function_score": {
-        #         u"query": {
-        #             u"bool": {
-        #                 u"filter": {
-        #                     u"bool": {
-        #                         u"must": [
-        #                             {u"bool": {u"should": [{u"term": {u"typeOfData": search_type}}]}},
-        #                             {u"bool": {u"should": [{u"term": {u"sectors": "Forestry"}}]}},
-        #                         ]
-        #                     }
-        #                 },
-        #             }
-        #         }
-        #     }
-        # }
-        #
-        # base_query = "/{0}/data-and-downloads/?lang={0}&source=".format(
-        #     self.current_lang)
-        # q = {"query": t}
-        # link = base_query + urllib.quote(json.dumps(q))
-        #
-        # return link
 
     def sections(self):
         catalog = get_tool("portal_catalog")
