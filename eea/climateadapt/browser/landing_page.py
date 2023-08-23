8fd2cd237b50889f6e86701341f03e52cde739d3
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2023-08-23 20:36:13 +0300

    Adjust urban and forestry pages

--- eea/climateadapt/browser/landing_page.py
+++ eea/climateadapt/browser/landing_page.py
@@ -1,4 +1,4 @@
-import json
+# import json
 import urllib
 from collections import namedtuple
 
@@ -7,6 +7,7 @@ from eea.climateadapt.translation.utils import (TranslationUtilsMixin,
 from plone.api.portal import get_tool
 from Products.Five.browser import BrowserView
 
+# from urlparse import parse_sql
 
 Section = namedtuple("Section", ["title", "count", "link", "icon_class"])
 
@@ -25,34 +26,67 @@ SEARCH_TYPES_ICONS = [
     ("TOOL", "Tools", "fa-wrench"),
 ]
 
+
+# filters[0][field]=issued.date&
+# filters[0][type]=any&
+# filters[0][values][0]=Last 5 years&
+# filters[1][field]=language&
+# filters[1][type]=any&
+# filters[1][values][0]=en&
+# filters[2][field]=items_count_language&
+# filters[2][values][0]=n_1_n&
+# filters[2][type]=all&
+# filters[3][field]=objectProvides&
+# filters[3][values][0]=Research and knowledge project&
+# filters[3][type]=any
+
+
+# filters[1][field]=sectors&
+# filters[1][type]=any&
+# filters[1][values][0]=Urban
+
+# filters[0][values][0]=Guidance&
+# filters[0][type]=any&
+# filters[0][field]=objectProvides&
+
+# filters%5B0%5D%5Bfield%5D=issued.date&filters%5B0%5D%5Btype%5D=any&filters%5B0%5D%5Bvalues%5D%5B0%5D=Last%205%20years&filters%5B1%5D%5Bfield%5D=language&filters%5B1%5D%5Btype%5D=any&filters%5B1%5D%5Bvalues%5D%5B0%5D=en&filters%5B2%5D%5Bfield%5D=items_count_language&filters%5B2%5D%5Bvalues%5D%5B0%5D=n_1_n&filters%5B2%5D%5Btype%5D=all&filters%5B3%5D%5Bfield%5D=cca_adaptation_sectors.keyword&filters%5B3%5D%5Bvalues%5D%5B0%5D=Health&filters%5B3%5D%5Btype%5D=any
+
+# filters[0][field]=issued.date&
+# filters[0][type]=any&
+# filters[0][values][0]=Last 5 years&
+# filters[1][field]=language&
+# filters[1][type]=any&
+# filters[1][values][0]=en&
+# filters[2][field]=items_count_language&
+# filters[2][values][0]=n_1_n&
+# filters[2][type]=all&
+# filters[3][field]=cca_adaptation_sectors.keyword&
+# filters[3][values][0]=Health&
+# filters[3][type]=any
+
+def filters_to_query(args):
+    res = []
+    for i, (name, val) in enumerate(args):
+        res.append(['filters[{0}][field]'.format(i), name])
+        res.append(['filters[{0}][type]'.format(i), 'any'])
+        res.append(['filters[{0}][values][0]'.format(i), val])
+
+    return urllib.urlencode(dict(res))
+
+
 class Urban(BrowserView, TranslationUtilsMixin):
 
     # TODO: implement cache using eea.cache
     # @cache
     def _make_link(self, search_type):
-        t = {
-            u"function_score": {
-                u"query": {
-                    u"bool": {
-                        u"filter": {
-                            u"bool": {
-                                u"must": [
-                                    {u"bool": {u"should":[{u"term": {u"typeOfData": search_type}}]}},
-                                    {u"bool": {u"should":[{u"term": {u"sectors": "Urban"}}]}},
-                                ]
-                            }
-                        },
-                    }
-                }
-            }
-        }
+        args = [
+            ('objectProvides', search_type),
+            ('cca_adaptation_sectors.keyword', "Urban"),
+        ]
+        query = filters_to_query(args)
+        link = "/{0}/data-and-downloads/?{1}".format(self.current_lang, query)
 
-        base_query = "/{0}/data-and-downloads/?lang={0}&source=".format(
-            self.current_lang)
-        q = {"query": t}
-        l = base_query + urllib.quote(json.dumps(q))
-
-        return l
+        return link
 
     def sections(self):
         catalog = get_tool("portal_catalog")
@@ -78,7 +112,7 @@ class Urban(BrowserView, TranslationUtilsMixin):
             tmp_types.append(data)
         return [
             Section(x[1], counts.get(x[0], 0), self._make_link(x[1]), x[2])
-            #for x in SEARCH_TYPES_ICONS
+            # for x in SEARCH_TYPES_ICONS
             for x in tmp_types
         ]
 
@@ -88,29 +122,38 @@ class Forest(BrowserView, TranslationUtilsMixin):
     # TODO: implement cache using eea.cache
     # @cache
     def _make_link(self, search_type):
-        t = {
-            u"function_score": {
-                u"query": {
-                    u"bool": {
-                        u"filter": {
-                            u"bool": {
-                                u"must": [
-                                    {u"bool": {u"should":[{u"term": {u"typeOfData": search_type}}]}},
-                                    {u"bool": {u"should":[{u"term": {u"sectors": "Forestry"}}]}},
-                                ]
-                            }
-                        },
-                    }
-                }
-            }
-        }
+        args = [
+            ('objectProvides', search_type),
+            ('cca_adaptation_sectors.keyword', "Forestry"),
+        ]
+        query = filters_to_query(args)
+        link = "/{0}/data-and-downloads/?{1}".format(self.current_lang, query)
 
-        base_query = "/{0}/data-and-downloads/?lang={0}&source=".format(
-            self.current_lang)
-        q = {"query": t}
-        l = base_query + urllib.quote(json.dumps(q))
+        return link
 
-        return l
+        # t = {
+        #     u"function_score": {
+        #         u"query": {
+        #             u"bool": {
+        #                 u"filter": {
+        #                     u"bool": {
+        #                         u"must": [
+        #                             {u"bool": {u"should": [{u"term": {u"typeOfData": search_type}}]}},
+        #                             {u"bool": {u"should": [{u"term": {u"sectors": "Forestry"}}]}},
+        #                         ]
+        #                     }
+        #                 },
+        #             }
+        #         }
+        #     }
+        # }
+        #
+        # base_query = "/{0}/data-and-downloads/?lang={0}&source=".format(
+        #     self.current_lang)
+        # q = {"query": t}
+        # link = base_query + urllib.quote(json.dumps(q))
+        #
+        # return link
 
     def sections(self):
         catalog = get_tool("portal_catalog")
@@ -136,6 +179,6 @@ class Forest(BrowserView, TranslationUtilsMixin):
             tmp_types.append(data)
         return [
             Section(x[1], counts.get(x[0], 0), self._make_link(x[1]), x[2])
-            #for x in SEARCH_TYPES_ICONS
+            # for x in SEARCH_TYPES_ICONS
             for x in tmp_types
         ]
