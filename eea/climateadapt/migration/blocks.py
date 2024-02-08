c9b4d725b7e7d7083e07fb33e24bebfe4ba2fb5f
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-08 16:43:54 +0200

    Make title block in folder listings

--- eea/climateadapt/migration/blocks.py
+++ eea/climateadapt/migration/blocks.py
@@ -3,6 +3,15 @@ from .utils import make_uid
 
 def make_title_block():
     uid = make_uid()
-    block = {"@type": "title", "hideContentType": True}
+    block = {
+        "@type": "title",
+        "copyrightIcon": "ri-copyright-line",
+        "hideContentType": True,
+        "hideCreationDate": True,
+        "hideDownloadButton": True,
+        "hideModificationDate": True,
+        "hidePublishingDate": True,
+        "styles": {},
+    }
 
     return [uid, block]
