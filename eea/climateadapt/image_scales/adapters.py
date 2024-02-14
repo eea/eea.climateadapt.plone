9ecf794417eb8c599024e96e3f4d118bc279a03c
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-02-14 18:18:57 +0200

    Retrieve scales from storage, current code always regenerates on get_scales()

--- eea/climateadapt/image_scales/adapters.py
+++ eea/climateadapt/image_scales/adapters.py
@@ -85,6 +85,7 @@ class ImageFieldScales(object):
         self.field = field
 
     def __call__(self):
+        # __import__("pdb").set_trace()
         image = self.field.get(self.context)
         if not image:
             return None
@@ -120,10 +121,27 @@ class ImageFieldScales(object):
             }
         ]
 
+    def _get_scale_from_storage(self, fieldname, width, height):
+        annot = getattr(self.context, "__annotations__", None)
+        if annot is None:
+            return None
+        storage = annot.get("plone.scale", None)
+        if storage is None:
+            return None
+        # TODO: use an algorithm to detect change of modified image
+        for scale in storage.values():
+            if (
+                scale["fieldname"] == fieldname and scale["width"] == width
+                # and scale["height"] == height
+            ):
+                # __import__("pdb").set_trace()
+                return scale
+
     def get_scales(self, field, width, height):
         """Get a dictionary of available scales for a particular image field,
-        with the actual dimensions (aspect ratio of the original image).
+        with the actual dimensions(aspect ratio of the original image).
         """
+        needs_reindex = False
         scales = {}
 
         for name, actual_width, actual_height in _get_scale_infos():
@@ -138,26 +156,44 @@ class ImageFieldScales(object):
                 # Get the scale info without actually generating the scale,
                 # nor any old-style HiDPI scales.
             try:
-                scale = self.images_view.scale(
-                    field.__name__,
-                    width=actual_width,
-                    height=actual_height,
+                # TODO: try to retrieve the scale from annotation storage
+                # current code will always write a scale here
+                scale = self._get_scale_from_storage(
+                    field.__name__, width=actual_width, height=actual_height
                 )
+                if scale is None:
+                    scale = self.images_view.scale(
+                        field.__name__,
+                        width=actual_width,
+                        height=actual_height,
+                    )
+                    needs_reindex = True
             except:  # TODO: hotfix for migration
                 scale = None
             if scale is None:
                 # If we cannot get a scale, it is probably a corrupt image.
                 continue
 
-            url = scale.url
-            actual_width = scale.width
-            actual_height = scale.height
+            # __import__("pdb").set_trace()
+            if isinstance(scale, dict):
+                ext = scale["mimetype"].split("/")[1]
+                url = "@@images/%s.%s" % (scale["uid"], ext)
+
+                actual_width = scale["width"]
+                actual_height = scale["height"]
+            else:
+                url = scale.url
+                actual_width = scale.width
+                actual_height = scale.height
 
             scales[name] = {
                 "download": self._scale_view_from_url(url),
                 "width": actual_width,
                 "height": actual_height,
             }
+
+        # if needs_reindex:
+        #     self.context.reindexObject()
 
         return scales
 
