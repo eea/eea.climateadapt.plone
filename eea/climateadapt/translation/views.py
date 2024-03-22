b0b3284300544dcb8765d859062621422e12a56f
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-03-22 14:58:30 +0200

    Add logging for html

--- eea/climateadapt/translation/views.py
+++ eea/climateadapt/translation/views.py
@@ -280,6 +280,8 @@ class TranslationCallback(BrowserView):
         file.seek(0)
         b64_str = file.read()
         html_translated = base64.decodestring(b64_str).decode("latin-1")
+        logging.info("HTML from etranslation")
+        logger.info(html_translated)
         site = portal.getSite()
         trans_obj_path = form.get("external-reference")
         if "https://" in trans_obj_path:
