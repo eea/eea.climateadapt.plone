11e26f919a6478be25e8227b44d147903d9f999a
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-04-08 11:22:12 +0300

    Add migrate content tree

--- eea/climateadapt/migration/views.py
+++ eea/climateadapt/migration/views.py
@@ -1,3 +1,5 @@
+import transaction
+from plone.api import content
 import logging
 
 from eea.climateadapt.migration.interfaces import IMigrateToVolto
@@ -17,8 +19,7 @@ edw_logger.setLevel(logging.ERROR)
 
 class MigrateContent(BrowserView):
     def __call__(self):
-        migrate = getMultiAdapter(
-            (self.context, self.request), IMigrateToVolto)
+        migrate = getMultiAdapter((self.context, self.request), IMigrateToVolto)
         try:
             migrate()
         except Exception:
@@ -48,5 +49,24 @@ class PostMigrationCleanup(BrowserView):
     def __call__(self):
         site = portal.get()
         post_migration_cleanup(site, self.request)
+
+        return "ok"
+
+
+class MigrateContentTree(BrowserView):
+    def __call__(self):
+        brains = content.find(context=self.context, depth=9999, unrestricted=True)
+
+        for i, brain in enumerate(brains):
+            obj = brain.getObject()
+            migrate = getMultiAdapter((obj, self.request), IMigrateToVolto)
+            try:
+                migrate()
+            except Exception:
+                logger.exception("Error in migrator")
+
+            i += 1
+            if i % 100 == 0:
+                transaction.savepoint()
 
         return "ok"
