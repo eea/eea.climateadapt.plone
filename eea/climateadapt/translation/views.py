0a76591c4794e992c30ce94cd5c13d96b198f7fb
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-04-26 10:40:17 +0300

    Code cleanup

--- eea/climateadapt/translation/views.py
+++ eea/climateadapt/translation/views.py
@@ -1,18 +1,9 @@
 """Translation views"""
 
-import cgi
 import base64
+import cgi
 import logging
 import os
-
-from bs4 import BeautifulSoup
-from plone.api import portal
-from plone.app.multilingual.manager import TranslationManager
-from plone.app.textfield.value import RichTextValue
-from Products.Five.browser import BrowserView
-from Products.statusmessages.interfaces import IStatusMessage
-from zope import event
-from zope.component import getMultiAdapter
 
 from eea.cache.event import InvalidateMemCacheEvent
 from eea.climateadapt.browser.admin import force_unlock
@@ -20,6 +11,12 @@ from eea.climateadapt.translation.volto import (
     get_content_from_html,
     translate_volto_html,
 )
+from plone.api import portal
+from plone.app.multilingual.manager import TranslationManager
+from Products.Five.browser import BrowserView
+from Products.statusmessages.interfaces import IStatusMessage
+from zope import event
+from zope.component import getMultiAdapter
 
 from . import (
     get_translation_key_values,
@@ -36,6 +33,9 @@ from .core import (
     save_field_data,
 )
 from .interfaces import ITranslationContext
+
+# from bs4 import BeautifulSoup
+# from plone.app.textfield.value import RichTextValue
 
 logger = logging.getLogger("eea.climateadapt.translation")
 env = os.environ.get
@@ -197,146 +197,146 @@ class TranslationCallback(BrowserView):
         logger.info("Html volto translation saved for %s",
                     trans_obj.absolute_url())
 
-    def save_tile_field(self, form):
-        """Save a simple text filed in a cover tile"""
-        field = form.get("field", None)
-        tile_id = form.get("tile_id", None)
-        if tile_id is not None and field is not None:
-            form.pop("uid", None)
-            form.pop("one_step", None)
-            form.pop("request-id", None)
-            trans_obj_path = form.get("external-reference")
-            form.pop("external-reference", None)
-            form.pop("target-language", None)
-            form.pop("field", None)
-            form.pop("source_lang", None)
-            form.pop("tile_id", None)
-            form.pop("is_cover", None)
-            translated = form.pop("translation", form.keys()[0]).strip()
-            # translated = translated.decode('latin-1')
+    # def save_tile_field(self, form):
+    #     """Save a simple text filed in a cover tile"""
+    #     field = form.get("field", None)
+    #     tile_id = form.get("tile_id", None)
+    #     if tile_id is not None and field is not None:
+    #         form.pop("uid", None)
+    #         form.pop("one_step", None)
+    #         form.pop("request-id", None)
+    #         trans_obj_path = form.get("external-reference")
+    #         form.pop("external-reference", None)
+    #         form.pop("target-language", None)
+    #         form.pop("field", None)
+    #         form.pop("source_lang", None)
+    #         form.pop("tile_id", None)
+    #         form.pop("is_cover", None)
+    #         translated = form.pop("translation", form.keys()[0]).strip()
+    #         # translated = translated.decode('latin-1')
+    #
+    #         tile_annot_id = "plone.tiles.data." + tile_id
+    #         site = portal.getSite()
+    #         if "https://" in trans_obj_path:
+    #             trans_obj_path = "/cca" + \
+    #                 trans_obj_path.split(site.absolute_url())[-1]
+    #         trans_obj = site.unrestrictedTraverse(trans_obj_path)
+    #         tile = trans_obj.__annotations__.get(tile_annot_id, None)
+    #
+    #         if not tile:
+    #             return
+    #
+    #         try:
+    #             update = tile.data
+    #         except AttributeError:
+    #             update = tile
+    #
+    #         translated_msg = translated
+    #         if translated_msg is not None:
+    #             update[field] = translated_msg
+    #
+    #         try:
+    #             trans_obj.__annotations__[tile_annot_id] = update
+    #         except Exception:
+    #             logger.info("One step: Error on saving translated tile field")
+    #             # import pdb; pdb.set_trace()
 
-            tile_annot_id = "plone.tiles.data." + tile_id
-            site = portal.getSite()
-            if "https://" in trans_obj_path:
-                trans_obj_path = "/cca" + \
-                    trans_obj_path.split(site.absolute_url())[-1]
-            trans_obj = site.unrestrictedTraverse(trans_obj_path)
-            tile = trans_obj.__annotations__.get(tile_annot_id, None)
+    # def save_text_field(self, uid, field, value, trans_obj_path):
+    #     """Save the translated value of given field for specified obj by uid"""
+    #     site = portal.getSite()
+    #     # catalog = site.portal_catalog
+    #     # trans_obj = get_translation_object_from_uid(uid, catalog)
+    #     trans_obj = site.unrestrictedTraverse(trans_obj_path)
+    #
+    #     if value is not None and value != "":
+    #         force_unlock(trans_obj)
+    #         encoded_text = value.encode("latin-1")
+    #         have_change = False
+    #
+    #         try:
+    #             setattr(trans_obj, field, encoded_text)
+    #             have_change = True
+    #         except AttributeError:
+    #             logger.info(
+    #                 "One step: AttributeError for obj: %s key: %s",
+    #                 trans_obj.absolute_url(),
+    #                 field,
+    #             )
+    #
+    #         if have_change:
+    #             trans_obj._p_changed = True
+    #             trans_obj.reindexObject()
+    #
+    #         logger.info("One step: saved %s %s %s", uid, field, value)
 
-            if not tile:
-                return
-
-            try:
-                update = tile.data
-            except AttributeError:
-                update = tile
-
-            translated_msg = translated
-            if translated_msg is not None:
-                update[field] = translated_msg
-
-            try:
-                trans_obj.__annotations__[tile_annot_id] = update
-            except Exception:
-                logger.info("One step: Error on saving translated tile field")
-                # import pdb; pdb.set_trace()
-
-    def save_text_field(self, uid, field, value, trans_obj_path):
-        """Save the translated value of given field for specified obj by uid"""
-        site = portal.getSite()
-        # catalog = site.portal_catalog
-        # trans_obj = get_translation_object_from_uid(uid, catalog)
-        trans_obj = site.unrestrictedTraverse(trans_obj_path)
-
-        if value is not None and value != "":
-            force_unlock(trans_obj)
-            encoded_text = value.encode("latin-1")
-            have_change = False
-
-            try:
-                setattr(trans_obj, field, encoded_text)
-                have_change = True
-            except AttributeError:
-                logger.info(
-                    "One step: AttributeError for obj: %s key: %s",
-                    trans_obj.absolute_url(),
-                    field,
-                )
-
-            if have_change:
-                trans_obj._p_changed = True
-                trans_obj.reindexObject()
-
-            logger.info("One step: saved %s %s %s", uid, field, value)
-
-    def save_html_fields(self, form, file):
-        """Get the translated html file, extract the values for each field and
-        update the related translation object.
-        """
-        site = portal.getSite()
-        trans_obj_path = form.get("external-reference")
-        if "https://" in trans_obj_path:
-            trans_obj_path = "/cca" + \
-                trans_obj_path.split(site.absolute_url())[-1]
-
-        form.pop("format")
-        form.pop("request-id")
-        form.pop("external-reference")
-        form.pop("source_lang")
-        form.pop("target-language")
-
-        # source_lang = form.get("source_lang")
-        # target_lang = form.get("target-language")
-        # logger.info("Translate %s to %s", source_lang, target_lang)
-
-        trans_obj = site.unrestrictedTraverse(trans_obj_path)
-        force_unlock(trans_obj)
-
-        if len(form.keys()) == 0:
-            logger.info("Empty form")  # TODO: Check why?
-            return
-
-        file.seek(0)
-        b64_str = file.read()
-        html_file = base64.decodestring(b64_str).decode("latin-1")
-        # logger.info(html_file)
-        # soup = BeautifulSoup(html_file, "html.parser")
-        soup = BeautifulSoup(html_file, "lxml")  # it's seems better
-        # for invalid HTML cases.
-
-        html_fields = soup.find_all(
-            "div", attrs={"class": "cca-translation-section"})
-
-        for field in html_fields:
-            field_name = field["data-field"]
-            html_value = field.decode_contents()
-            encoded_text = html_value.encode("latin-1")
-            setattr(trans_obj, field_name, RichTextValue(encoded_text))
-            trans_obj._p_changed = True
-            trans_obj.reindexObject(idxs=[field_name])
-
-        tiles = soup.find_all("div", attrs={"class": "cca-translation-tile"})
-
-        for field in tiles:
-            field_name = field["data-field"]
-            tile_id = field["data-tile-id"]
-            html_value = field.decode_contents()
-            encoded_text = html_value.encode("latin-1")
-            # tile = trans_obj.get_tile(tile_id)
-            tile_annot_id = "plone.tiles.data." + tile_id
-            tile = trans_obj.__annotations__.get(tile_annot_id, None)
-            if tile is not None:
-                try:
-                    update = tile.data
-                except AttributeError:
-                    update = tile
-                update["text"] = RichTextValue(encoded_text)
-                # tile.data.update(update)
-                trans_obj.__annotations__[tile_annot_id] = update
-                # trans_obj.reindexObject()
-            else:
-                logger.info("Cannot find tile")
-        logger.info("Html translation saved for %s", trans_obj.absolute_url())
+    # def save_html_fields(self, form, file):
+    #     """Get the translated html file, extract the values for each field and
+    #     update the related translation object.
+    #     """
+    #     site = portal.getSite()
+    #     trans_obj_path = form.get("external-reference")
+    #     if "https://" in trans_obj_path:
+    #         trans_obj_path = "/cca" + \
+    #             trans_obj_path.split(site.absolute_url())[-1]
+    #
+    #     form.pop("format")
+    #     form.pop("request-id")
+    #     form.pop("external-reference")
+    #     form.pop("source_lang")
+    #     form.pop("target-language")
+    #
+    #     # source_lang = form.get("source_lang")
+    #     # target_lang = form.get("target-language")
+    #     # logger.info("Translate %s to %s", source_lang, target_lang)
+    #
+    #     trans_obj = site.unrestrictedTraverse(trans_obj_path)
+    #     force_unlock(trans_obj)
+    #
+    #     if len(form.keys()) == 0:
+    #         logger.info("Empty form")  # TODO: Check why?
+    #         return
+    #
+    #     file.seek(0)
+    #     b64_str = file.read()
+    #     html_file = base64.decodestring(b64_str).decode("latin-1")
+    #     # logger.info(html_file)
+    #     # soup = BeautifulSoup(html_file, "html.parser")
+    #     soup = BeautifulSoup(html_file, "lxml")  # it's seems better
+    #     # for invalid HTML cases.
+    #
+    #     html_fields = soup.find_all(
+    #         "div", attrs={"class": "cca-translation-section"})
+    #
+    #     for field in html_fields:
+    #         field_name = field["data-field"]
+    #         html_value = field.decode_contents()
+    #         encoded_text = html_value.encode("latin-1")
+    #         setattr(trans_obj, field_name, RichTextValue(encoded_text))
+    #         trans_obj._p_changed = True
+    #         trans_obj.reindexObject(idxs=[field_name])
+    #
+    #     tiles = soup.find_all("div", attrs={"class": "cca-translation-tile"})
+    #
+    #     for field in tiles:
+    #         field_name = field["data-field"]
+    #         tile_id = field["data-tile-id"]
+    #         html_value = field.decode_contents()
+    #         encoded_text = html_value.encode("latin-1")
+    #         # tile = trans_obj.get_tile(tile_id)
+    #         tile_annot_id = "plone.tiles.data." + tile_id
+    #         tile = trans_obj.__annotations__.get(tile_annot_id, None)
+    #         if tile is not None:
+    #             try:
+    #                 update = tile.data
+    #             except AttributeError:
+    #                 update = tile
+    #             update["text"] = RichTextValue(encoded_text)
+    #             # tile.data.update(update)
+    #             trans_obj.__annotations__[tile_annot_id] = update
+    #             # trans_obj.reindexObject()
+    #         else:
+    #             logger.info("Cannot find tile")
+    #     logger.info("Html translation saved for %s", trans_obj.absolute_url())
 
 
 class TranslationList(BrowserView):
