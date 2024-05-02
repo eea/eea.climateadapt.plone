cc13171f34d983d3a89b9a19bfa064d8c52631b2
Tiberiu Ichim <tiberiuichim@users.noreply.github.com>
2024-05-02 11:13:21 +0300

    Develop (#140)
    * Don't serialize the results in the block if items exist
    
    * Nav title (#139)
    
    * Add navtitle behavior
    
    * Customize navigation to use nav_title in portal tabs

---
+++ eea/climateadapt/behaviors/navtitle.py
@@ -0,0 +1,11 @@
+from plone.autoform.interfaces import IFormFieldProvider
+from plone.supermodel import model
+from eea.climateadapt import MessageFactory as _
+from zope.interface import provider
+from zope.schema import TextLine
+
+
+@provider(IFormFieldProvider)
+class INavTitle(model.Schema):
+
+    nav_title = TextLine(title=_("Navigation title"), required=False)
