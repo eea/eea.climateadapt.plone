3fc10e6660f0fe6475ebffad7e9cefec3b3111c9
iugin <tripon.eugen@gmail.com>
2024-10-07 11:52:38 +0300

    Refs #273823 - seo options robots meta (#169)
    * Refs #273823 - seo options robots meta
    
    * Simplify
    
    * Simplify
    
    ---------
    
    Co-authored-by: Tiberiu Ichim <tiberiu.ichim@gmail.com>

--- eea/climateadapt/interfaces.py
+++ eea/climateadapt/interfaces.py
@@ -26,6 +26,7 @@ class ITransnationalRegionMarker(Interface):
     """ A marker interface for transnational region pages.
     """
 
+
 class IMainTransnationalRegionMarker(Interface):
     """ A marker interface for a main transnational region pages.
     """
