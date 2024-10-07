fd90ec3dde4922a3b324a229f9df1afac0969029
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-10-07 11:45:57 +0300

    Simplify

--- eea/climateadapt/interfaces.py
+++ eea/climateadapt/interfaces.py
@@ -118,32 +118,3 @@ class ICCAContentTypesSettings(Interface):
         value_type=schema.Choice(
             vocabulary="plone.app.vocabularies.ReallyUserFriendlyTypes")
     )
-
-
-class ISEOFieldsMarker(Interface):
-    """Marker interface that will be provided by instances using the
-    ISeoFields behavior.
-    """
-
-
-class ICollectiveBehaviorSeoLayer(IDefaultBrowserLayer):
-    """Marker interface that defines a browser layer."""
-
-
-class ICollectiveBehaviorSeoSettings(Interface):
-
-    robot_tags = schema.Tuple(
-        title=_("Robot Tags"),
-        description=_(
-            "robot_tags_desc",
-            default=(
-                "Enter combinations of robot tags, separated by comma's one "
-                "combination on every line. The idea here is that the "
-                "webmaster decides which combinations of multiple tags are "
-                "sane, so that content editors don't need to compose logical "
-                "combinations themselves from all individual tags."
-            ),
-        ),
-        value_type=schema.TextLine(),
-        default=("index, nofollow", "noindex, follow", "index, follow"),
-    )
