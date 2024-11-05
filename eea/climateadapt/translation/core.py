8a4072f25c5a8d4ce48cb4be64655ec13eb5261d
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-11-05 12:05:36 +0200

    Add safe_traverse

--- eea/climateadapt/translation/core.py
+++ eea/climateadapt/translation/core.py
@@ -203,6 +203,24 @@ def copy_missing_interfaces(en_obj, trans_obj):
             logger.info("Copied interface: %s" % interf[0])
 
 
+def safe_traverse(obj, trans_path):
+    parts = trans_path.strip("/").split("/")
+    current_obj = obj
+
+    for part in parts:
+        if hasattr(current_obj, "objectIds") and callable(current_obj.objectIds):
+            if part in current_obj.objectIds():
+                current_obj = current_obj[part]
+            else:
+                return None
+        # elif hasattr(current_obj, part):
+        #     current_obj = getattr(current_obj, part)
+        else:
+            return None
+
+    return current_obj
+
+
 def setup_translation_object(obj, language, site_portal):
     """Create translation object for an obj"""
 
@@ -230,7 +248,7 @@ def setup_translation_object(obj, language, site_portal):
 
     trans = None
     try:
-        trans = obj.unrestrictedTraverse(trans_path)
+        trans = safe_traverse(obj, trans_path)
     except Exception:
         pass
 
@@ -243,6 +261,7 @@ def setup_translation_object(obj, language, site_portal):
             # trans_path,
         )
         trans.reindexObject()
+
         return trans
 
     check_ancestors_path_exists(obj, language, site_portal)
