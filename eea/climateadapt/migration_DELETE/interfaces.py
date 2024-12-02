5658ba34264042924b560719e5b872b31c929917
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2024-12-02 15:31:13 +0200

    Remove importer and migration

--- eea/climateadapt/migration_DELETE/interfaces.py
+++
@@ -1,9 +0,0 @@
-from zope.interface import Interface
-
-
-class IMigrateToVolto(Interface):
-    """Migrate content to Volto blocks"""
-
-
-class IMigrateTile(Interface):
-    """Migrate a collective.cover tile"""
