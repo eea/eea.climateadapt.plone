95c475657d87603932ad643f5ca37106ec2e50aa
laszlocseh <csehlaszlo91@gmail.com>
2024-12-03 15:33:24 +0200

    enable AceVideo

--- eea/climateadapt/acevideo.py
+++ eea/climateadapt/acevideo.py
@@ -1,12 +1,12 @@
-# from zope.interface import implementer
+from zope.interface import implementer
 
-# from eea.climateadapt.behaviors import IAceVideo
-# from eea.climateadapt.interfaces import IClimateAdaptContent
-# from plone.dexterity.content import Container
+from eea.climateadapt.behaviors import IAceVideo
+from eea.climateadapt.interfaces import IClimateAdaptContent
+from plone.dexterity.content import Container
 
 
-# @implementer(IAceVideo, IClimateAdaptContent)
-# class Video(Container):
-#     """A video content type implementation"""
+@implementer(IAceVideo, IClimateAdaptContent)
+class Video(Container):
+    """A video content type implementation"""
 
-#     search_type = "VIDEO"
+    search_type = "VIDEO"
