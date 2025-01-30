30769569149b8f5f5301a738d45bfd3356dfc74f
laszlocseh <csehlaszlo91@gmail.com>
2025-01-30 13:01:13 +0200

    fix for data_type field in fix_content

--- eea/climateadapt/scripts/fix_content.py
+++ eea/climateadapt/scripts/fix_content.py
@@ -12,7 +12,7 @@ logger = logging.getLogger("fixer")
 
 REPLACED_URL = 'http://10.50.5.50:8060/cca'
 REPLACE_WITH = 'https://climate-adapt-plone6.devel5cph.eea.europa.eu'
-START_FROM = 89400
+START_FROM = 92200
 
 def get_blocks(obj):
     """get_blocks"""
@@ -342,6 +342,7 @@ def fix_data_type(obj):
         "NARZĘDZIE": "TOOL",
         "LEITLINIEN": "GUIDANCE",
         "INDIKATOR": "INDICATOR",
+        "ORGANISATION, EINRICHTUNG": "ORGANISATION",
     }
     data_type = obj.get("data_type", None)
 
