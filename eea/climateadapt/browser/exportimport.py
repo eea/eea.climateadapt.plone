2d7a03865b74e8c888282fd48be7e9d277f07a31
laszlocseh <csehlaszlo91@gmail.com>
2025-02-07 11:40:29 +0200

    add annotation keys to be exported

--- eea/climateadapt/browser/exportimport.py
+++ eea/climateadapt/browser/exportimport.py
@@ -19,7 +19,8 @@ from eea.climateadapt.interfaces import (
 from zope.annotation.interfaces import IAnnotations
 
 ANNOTATIONS_TO_EXPORT = [
-    "",
+    "c3s_json_data",
+    "broken_links_data"
 ]
 ANNOTATIONS_KEY = "exportimport.annotations"
 
