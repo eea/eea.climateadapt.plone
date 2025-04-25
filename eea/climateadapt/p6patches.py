0f7eea6cf251785383f51cd39c1e870b44eeec6d
kreafox <krisztina.n.elekes@gmail.com>
2025-04-25 13:02:28 +0300

    Update event date fields description

--- eea/climateadapt/p6patches.py
+++ eea/climateadapt/p6patches.py
@@ -6,17 +6,14 @@ logger = getLogger("eea.climateadapt")
 
 def install_patches():
     IEventBasic["start"].description = (
-        "Date and Time, when the event begins. (When editing an event, "
-        "the displayed date and time here is adjusted to your local timezone. "
-        "Be careful, as changes may shift the event date and time if it was "
-        "originally set in a different timezone)"
+        "Date and time when the event begins (in your local timezone). "
+        "For example, if you're in Copenhagen and creating an event for Athens, "
+        "use Copenhagen time, not Athens time."
     )
     IEventBasic["end"].description = (
-        "Date and Time, when the event ends. (When editing an event, "
-        "the displayed date and time here is adjusted to your local timezone. "
-        "Be careful, as changes may shift the event date and time if it was "
-        "originally set in a different timezone)"
+        "Date and time when the event ends (in your local timezone). "
+        "For example, if you're in Copenhagen and creating an event for Athens, "
+        "use Copenhagen time, not Athens time."
     )
 
     logger.info("Patched IEventBasic descriptions")
-
