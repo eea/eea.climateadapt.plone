d7df7eeeb34e146ae088a12c5347cee9226c3645
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-05-20 11:58:58 +0300

    Add config logger

--- eea/climateadapt/p6patches.py
+++ eea/climateadapt/p6patches.py
@@ -1,5 +1,6 @@
 from logging import getLogger
 from plone.app.event.dx.behaviors import IEventBasic
+import logging.config
 
 logger = getLogger("eea.climateadapt")
 
@@ -17,3 +18,29 @@ def install_patches():
     )
 
     logger.info("Patched IEventBasic descriptions")
+
+
+logging_config = {
+    "version": 1,
+    "disable_existing_loggers": False,
+    "formatters": {
+        "standard": {"format": "%(asctime)s [%(levelname)s] %(name)s: %(message)s"},
+    },
+    "handlers": {
+        "console": {
+            "level": "INFO",
+            "formatter": "standard",
+            "class": "logging.StreamHandler",
+        },
+    },
+    "loggers": {
+        "plone.purgecaching.purger": {
+            "handlers": ["console"],
+            "level": "INFO",
+            "propagate": False,
+        },
+    },
+}
+
+# Apply the logging configuration
+logging.config.dictConfig(logging_config)
