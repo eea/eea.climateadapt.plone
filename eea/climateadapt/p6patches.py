7601a482c957ca89740207d68b694af0ab40e284
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-05-21 15:27:36 +0300

    Tweak purging URLs

--- eea/climateadapt/p6patches.py
+++ eea/climateadapt/p6patches.py
@@ -1,3 +1,4 @@
+from plone.restapi.cache import paths
 from plone.cachepurging.purger import logger as purgeLogger
 from plone.app.event.dx.behaviors import IEventBasic
 import logging
@@ -19,5 +20,11 @@ def install_patches():
         "For example, if you're in Copenhagen and creating an event for Athens, "
         "use Copenhagen time, not Athens time."
     )
-
     logger.info("Patched IEventBasic descriptions")
+
+    paths.CONTEXT_ENDPOINTS = [
+        "?expand=translations,subsite,breadcrumbs,navigation,actions&expand.navigation.depth=3",
+        "/@types",
+    ]
+
+    logger.info("plone.restapi cache purging paths were setup for CCA")
