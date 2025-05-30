a235288d4e869270152aa599302a6382f7447370
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-05-30 10:18:57 +0300

    Logging

--- eea/climateadapt/translation/events.py
+++ eea/climateadapt/translation/events.py
@@ -1,4 +1,7 @@
+import logging
 from .core import queue_job
+
+logger = logging.getLogger("eea.climateadapt")
 
 
 def object_modified_handler(obj, event):
@@ -6,7 +9,17 @@ def object_modified_handler(obj, event):
     Event handler for ObjectModifiedEvent.
     This handler will be called whenever a content object is modified.
     """
+
     op = "/".join(event.oldParent.getPhysicalPath())
+    np = "/".join(event.newParent.getPhysicalPath())
+
+    logger.info(
+        "Object moved %s -> %s (%s -> %s)",
+        op,
+        np,
+        event.oldName,
+        event.newName,
+    )
 
     if "/en/" not in op:
         return
