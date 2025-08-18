ed4d8263feebc3988420a8e027d62f7e5cc49c3b
Tiberiu Ichim <tiberiu.ichim@gmail.com>
2025-08-18 18:53:36 +0300

    Add module

---
+++ eea/climateadapt/restapi/navroot.py
@@ -0,0 +1,47 @@
+from plone.restapi.interfaces import IExpandableElement
+from plone.restapi.interfaces import ISerializeToJsonSummary  # ISerializeToJson,
+from plone.restapi.services import Service
+from zope.component import adapter
+from zope.component import getMultiAdapter
+from zope.interface import implementer
+from zope.interface import Interface
+from eea.volto.policy.interfaces import IEeaVoltoPolicyLayer
+
+
+@implementer(IExpandableElement)
+@adapter(Interface, IEeaVoltoPolicyLayer)
+class Navroot:
+    def __init__(self, context, request):
+        self.context = context
+        self.request = request
+
+    def __call__(self, expand=False):
+        result = {"navroot": {"@id": f"{self.context.absolute_url()}/@navroot"}}
+        if not expand:
+            return result
+
+        portal_state = getMultiAdapter(
+            (self.context, self.request), name="plone_portal_state"
+        )
+        # We need to unset expansion here, otherwise we get infinite recursion
+        self.request.form["expand"] = ""
+
+        try:
+            result["navroot"]["navroot"] = getMultiAdapter(
+                (portal_state.navigation_root(), self.request),
+                ISerializeToJsonSummary,
+            )(include_items=False, include_expansion=False)
+        except TypeError:
+            # Handle adapters that do not support keyword args gracefully.
+            result["navroot"]["navroot"] = getMultiAdapter(
+                (portal_state.navigation_root(), self.request),
+                ISerializeToJsonSummary,
+            )()
+
+        return result
+
+
+class NavrootGet(Service):
+    def reply(self):
+        navroot = Navroot(self.context, self.request)
+        return navroot(expand=True)["navroot"]
