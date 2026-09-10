aa854f997192f167c0026db90322f92769c2eb72
eea-jenkins <eea-github@googlegroups.com>
2026-09-10 12:18:20 +0200

    style: Automated code fix

--- eea/climateadapt/tool_import.py
+++ eea/climateadapt/tool_import.py
@@ -1061,9 +1061,7 @@ class ExtendedToolsImporter:
             if hasattr(container, "getPhysicalPath")
             else str(container)
         )
-        logger.info(
-            "Target import container: %s (dry_run=%s)", container_path, dry_run
-        )
+        logger.info("Target import container: %s (dry_run=%s)", container_path, dry_run)
 
         if not dry_run:
             try:
