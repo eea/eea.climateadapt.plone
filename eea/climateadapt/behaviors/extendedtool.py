31afa338c097d30915d0f11120b420aca48c9506
kreafox <krisztina.n.elekes@gmail.com>
2026-09-14 11:23:31 +0300

    fix: change sectors field requirement to optional

--- eea/climateadapt/behaviors/extendedtool.py
+++ eea/climateadapt/behaviors/extendedtool.py
@@ -24,7 +24,7 @@ class IExtendedTool(ITool, IBlocks):
         description=_(
             "Select one or more relevant sector policies that this item relates to."
         ),
-        required=True,
+        required=False,
         missing_value=[],
         default=None,
         value_type=Choice(
