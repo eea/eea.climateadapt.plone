9639950c08436fd26d51bf5cd2e11afbb26c5d23
kreafox <krisztina.n.elekes@gmail.com>
2026-09-14 10:48:24 +0300

    fix: move focus areas field to extended tool layout blocks

--- eea/climateadapt/behaviors/volto_layout.py
+++ eea/climateadapt/behaviors/volto_layout.py
@@ -2388,14 +2388,6 @@ tool_layout_blocks = {
                                     },
                                 },
                                 {
-                                    "@id": "5885250a-c9fb-4f3e-b3bc-d850cc19097b",
-                                    "field": {
-                                        "id": "focus_areas",
-                                        "title": "Focus areas",
-                                        "widget": "array",
-                                    },
-                                },
-                                {
                                     "@id": "7a399cd9-7b68-4127-bc72-1bcc1ec86be2",
                                     "field": {
                                         "id": "elements",
@@ -2688,6 +2680,14 @@ extendetool_layout_blocks = {
                                     "field": {
                                         "id": "climate_impacts",
                                         "title": "Climate impacts",
+                                        "widget": "array",
+                                    },
+                                },
+                                {
+                                    "@id": "5885250a-c9fb-4f3e-b3bc-d850cc19097b",
+                                    "field": {
+                                        "id": "focus_areas",
+                                        "title": "Focus areas",
                                         "widget": "array",
                                     },
                                 },
